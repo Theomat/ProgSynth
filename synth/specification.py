09fa2483ed8c2247015ebe876e3a0b94b093e5d1
Theomat <theomatricon@gmail.com>
2021-12-15 20:56:40 +0100

    Added a guess_type function from Example

--- synth/specification.py
+++ synth/specification.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import Any, List
 
+from synth.syntax.type_system import FunctionType, Type, guess_type
+
 
 class TaskSpecification:
     pass
@@ -11,7 +13,14 @@ class Example:
     inputs: List[Any]
     output: Any
 
+    def guess_type(self) -> Type:
+        types = list(map(guess_type, self.inputs)) + [guess_type(self.output)]
+        return FunctionType(*types)
+
 
 @dataclass
 class PBE(TaskSpecification):
     examples: List[Example]
+
+    def guess_type(self) -> Type:
+        return self.examples[0].guess_type()
