b7371d6d5142c487e233752e1f964795d2df9f6c
Theomat <theomatricon@gmail.com>
2021-12-15 17:37:36 +0100

    Fixed formatting

--- synth/generation/sampler.py
+++ synth/generation/sampler.py
@@ -35,16 +35,21 @@ class LexiconSampler(Sampler[U]):
 
 
 class ListSampler(Sampler[TList]):
-    def __init__(self, element_sampler: Sampler[U], probabilities: Union[TList[float], TList[Tuple[int, float]]], max_depth: int = -1, seed: Optional[int] = None) -> None:
+    def __init__(
+        self,
+        element_sampler: Sampler[U],
+        probabilities: Union[TList[float], TList[Tuple[int, float]]],
+        max_depth: int = -1,
+        seed: Optional[int] = None,
+    ) -> None:
         super().__init__()
         self.max_depth = max_depth
         self.element_sampler = element_sampler
-        correct_prob: TList[Tuple[int, float]] = probabilities #type: ignore
+        correct_prob: TList[Tuple[int, float]] = probabilities  # type: ignore
         if not isinstance(probabilities[0], tuple):
-            correct_prob = [(i + 1, p) for i, p in enumerate(probabilities)] #type: ignore
+            correct_prob = [(i + 1, p) for i, p in enumerate(probabilities)]  # type: ignore
         self._length_mapping = [n for n, _ in correct_prob]
         self.sampler = vose.Sampler(np.array([p for _, p in correct_prob]), seed=seed)
-
 
     def sample(self, type: Type, **kwargs: Any) -> TList:
         assert self.max_depth < 0 and type.depth() <= self.max_depth
