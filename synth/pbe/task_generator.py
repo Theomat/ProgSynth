23902bbddb922e5811717533b815e12329b2cc59
Theomat <theomatricon@gmail.com>
2021-12-20 19:10:59 +0100

    Adapted & added reproduce_dataset function

--- synth/pbe/task_generator.py
+++ synth/pbe/task_generator.py
@@ -1,12 +1,22 @@
-from typing import Callable, Dict, Generator, List as TList, Any
+from typing import Callable, Dict, Generator, List as TList, Any, Optional
 
-from synth.task import Task
+import numpy as np
+
+from synth.task import Dataset, Task
 from synth.specification import PBE, Example
 from synth.semantic.evaluator import Evaluator
-from synth.syntax.type_system import Arrow, Type
+from synth.syntax.dsl import DSL
 from synth.syntax.program import Program
+from synth.syntax.type_system import BOOL, INT, Arrow, List, Type
+from synth.syntax.concrete.concrete_cfg import ConcreteCFG
 from synth.syntax.concrete.concrete_pcfg import ConcretePCFG
-from synth.generation.sampler import Sampler
+from synth.generation.sampler import (
+    LexiconSampler,
+    ListSampler,
+    RequestSampler,
+    Sampler,
+    UnionSampler,
+)
 
 
 class TaskGenerator:
@@ -14,8 +24,8 @@ class TaskGenerator:
         self,
         input_generator: Sampler,
         evaluator: Evaluator,
-        gen_random_type_request: Callable[[], Type],
-        gen_random_sample_number: Callable[[Type], int],
+        gen_random_type_request: Sampler[Type],
+        gen_random_sample_number: Sampler[int],
         type2pcfg: Dict[Type, ConcretePCFG],
         output_validator: Callable[[Any], bool],
         max_tries: int = 100,
@@ -40,7 +50,7 @@ class TaskGenerator:
         return solution
 
     def generate_task(self) -> Task[PBE]:
-        type_request = self.gen_random_type_request()
+        type_request = self.gen_random_type_request.sample()
         arguments = (
             [] if not isinstance(type_request, Arrow) else type_request.arguments()
         )
@@ -50,7 +60,7 @@ class TaskGenerator:
         # Generate correct program that makes use of all variables
         solution = self.__generate_program__(type_request)
         # Try to generate the required number of samples
-        samples = self.gen_random_sample_number(type_request)
+        samples = self.gen_random_sample_number.sample(type=type_request)
         inputs: TList = []
         outputs = []
         tries = 0
@@ -60,7 +70,7 @@ class TaskGenerator:
         ) >= samples and tries < self.max_tries:
             tries += 1
             new_input = [
-                self.input_generator.sample(arg_type) for arg_type in arguments
+                self.input_generator.sample(type=arg_type) for arg_type in arguments
             ]
             try:
                 output = self.evaluator.eval(solution, new_input)
@@ -106,3 +116,132 @@ def basic_output_validator(
         return False
 
     return validate_output
+
+
+def reproduce_dataset(
+    dataset: Dataset[PBE],
+    dsl: DSL,
+    evaluator: Evaluator,
+    seed: Optional[int] = None,
+    max_tries: int = 100,
+) -> TaskGenerator:
+
+    max_depth = -1
+    allowed_types: TList[Type] = []
+    types_probs_list: TList[float] = []
+
+    list_length: Dict[Type, Dict[int, int]] = {}
+    no_samples: Dict[Type, Dict[int, int]] = {}
+    max_list_depth = [0]
+
+    int_range: TList[int] = [999999999, 0]
+    int_range[1] = -int_range[0]
+
+    def analyze(element: Any, type: Type, depth: int = 0) -> None:
+        if depth > max_list_depth[0]:
+            max_list_depth[0] = depth
+        if isinstance(type, List):
+            elt_type = type.element_type
+            if len(element) > 0:
+                __multi_discrete_distribution__(list_length, elt_type, len(element))
+                for el in element:
+                    analyze(el, elt_type, depth + 1)
+        elif element:
+            int_range[0] = min(int_range[0], element)
+            int_range[1] = max(int_range[1], element)
+
+    # Capture all information in one dataset pass
+    for task in dataset:
+        if task.solution:
+            max_depth = max(max_depth, task.solution.depth())
+        # Type distribution
+        if task.type_request not in allowed_types:
+            allowed_types.append(task.type_request)
+            types_probs_list.append(1.0)
+        else:
+            index = allowed_types.index(task.type_request)
+            types_probs_list[index] += 1.0
+
+        # No samples distribution
+        __multi_discrete_distribution__(
+            no_samples, task.type_request, len(task.specification.examples)
+        )
+
+        t = task.type_request
+        args = [] if not isinstance(t, Arrow) else t.arguments()
+        r = t if not isinstance(t, Arrow) else t.returns()
+
+        # Input data analysis
+        for ex in task.specification.examples:
+
+            for input, ti in zip(ex.inputs, args):
+                analyze(input, ti)
+            analyze(ex.output, r)
+
+    # Type generator
+    types_probs = np.array(types_probs_list, dtype=float) / len(dataset)
+    type_sampler = LexiconSampler(allowed_types, types_probs, seed)
+
+    list_length_gen = __multi_discrete_to_gen__(list_length, seed=seed)
+    no_samples_gen = __multi_discrete_to_gen__(no_samples, seed=seed)
+
+    int_lexicon = list(range(int_range[0], int_range[1] + 1))
+
+    type2PCFG = {
+        t: ConcretePCFG.uniform_from_cfg(ConcreteCFG.from_dsl(dsl, t, max_depth))
+        for t in allowed_types
+    }
+    for pcfg in type2PCFG.values():
+        pcfg.init_sampling(seed)
+
+    input_sampler = ListSampler(
+        UnionSampler(
+            {
+                INT: LexiconSampler(int_lexicon, seed=seed),
+                BOOL: LexiconSampler([True, False], seed=seed),
+            }
+        ),
+        list_length_gen,
+        max_depth=max_list_depth[0],
+        seed=seed,
+    )
+
+    return TaskGenerator(
+        input_sampler,
+        evaluator,
+        type_sampler,
+        no_samples_gen,
+        type2PCFG,
+        basic_output_validator(
+            int_lexicon, max(max(l.keys()) for l in list_length.values())
+        ),
+        max_tries,
+    )
+
+
+def __multi_discrete_distribution__(
+    distr: Dict[Type, Dict[int, int]], key: Type, new_entry: int
+) -> None:
+    if key not in distr:
+        distr[key] = {new_entry: 0}
+    elif new_entry not in distr[key]:
+        distr[key][new_entry] = 0
+    distr[key][new_entry] += 1
+
+
+def __multi_discrete_to_gen__(
+    distr: Dict[Type, Dict[int, int]], seed: Optional[int] = None
+) -> RequestSampler[int]:
+
+    choice_map: Dict[Type, TList[int]] = {k: list(v.keys()) for k, v in distr.items()}
+    probs_map: Dict[Type, np.ndarray] = {
+        k: np.array(list(v.values()), dtype=float) for k, v in distr.items()
+    }
+    for k, v in probs_map.items():
+        probs_map[k] /= np.sum(v)
+
+    samplers: Dict[Type, Sampler] = {
+        k: LexiconSampler(choice_map[k], probs_map[k], seed=seed)
+        for k in probs_map.keys()
+    }
+    return UnionSampler(samplers)
