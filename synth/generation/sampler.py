dff77bb36c56fbad6160b719bdf8785179404266
Theomat <theomatricon@gmail.com>
2021-12-20 19:10:30 +0100

    ListSampler can sample length conditionally on type

--- synth/generation/sampler.py
+++ synth/generation/sampler.py
@@ -1,5 +1,15 @@
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Generic, Iterable, List as TList, Optional, Tuple, TypeVar, Union
+from typing import (
+    Any,
+    Dict,
+    Generic,
+    Iterable,
+    List as TList,
+    Optional,
+    Tuple,
+    TypeVar,
+    Union,
+)
 import copy
 
 import numpy as np
@@ -47,18 +57,33 @@ class ListSampler(RequestSampler[Union[TList, U]]):
     def __init__(
         self,
         element_sampler: Sampler[U],
-        probabilities: Union[TList[float], TList[Tuple[int, float]]],
+        probabilities: Union[
+            TList[float], TList[Tuple[int, float]], RequestSampler[int]
+        ],
         max_depth: int = -1,
         seed: Optional[int] = None,
     ) -> None:
         super().__init__()
         self.max_depth = max_depth
         self.element_sampler = element_sampler
-        correct_prob: TList[Tuple[int, float]] = probabilities  # type: ignore
-        if not isinstance(probabilities[0], tuple):
-            correct_prob = [(i + 1, p) for i, p in enumerate(probabilities)]  # type: ignore
-        self._length_mapping = [n for n, _ in correct_prob]
-        self.sampler = vose.Sampler(np.array([p for _, p in correct_prob]), seed=seed)
+        if isinstance(probabilities, RequestSampler):
+            self.length_sampler = probabilities
+            self.sampler = None
+        else:
+            correct_prob: TList[Tuple[int, float]] = probabilities  # type: ignore
+            if not isinstance(probabilities[0], tuple):
+                correct_prob = [(i + 1, p) for i, p in enumerate(probabilities)]  # type: ignore
+            self._length_mapping = [n for n, _ in correct_prob]
+            self.sampler = vose.Sampler(
+                np.array([p for _, p in correct_prob]), seed=seed
+            )
+
+    def __gen_length__(self, type: Type) -> int:
+        if self.sampler:
+            i: int = self.sampler.sample()
+            return self._length_mapping[i]
+        else:
+            return self.length_sampler.sample(type=type)
 
     def sample_for(self, type: Type, **kwargs: Any) -> Union[TList, U]:
         assert self.max_depth < 0 or type.depth() <= self.max_depth
@@ -66,7 +91,7 @@ class ListSampler(RequestSampler[Union[TList, U]]):
             sampler: Sampler = self
             if not isinstance(type.element_type, List):
                 sampler = self.element_sampler
-            length: int = self._length_mapping[self.sampler.sample()]
+            length: int = self.__gen_length__(type)
             return [
                 sampler.sample(type=type.element_type, **kwargs) for _ in range(length)
             ]
