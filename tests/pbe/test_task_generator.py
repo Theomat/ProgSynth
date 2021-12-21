4d97ac51adad6eff75c9f69517308788ad149f75
Theomat <theomatricon@gmail.com>
2021-12-21 16:14:38 +0100

    Added task generator test

--- tests/pbe/test_task_generator.py
+++ tests/pbe/test_task_generator.py
@@ -1,1 +1,81 @@
-# TODO
+from synth.generation.sampler import LexiconSampler
+from synth.pbe.task_generator import TaskGenerator, basic_output_validator
+from synth.semantic.evaluator import DSLEvaluator
+from synth.syntax.concrete.concrete_cfg import ConcreteCFG
+from synth.syntax.concrete.concrete_pcfg import ConcretePCFG
+from synth.syntax.dsl import DSL
+from synth.syntax.type_system import (
+    INT,
+    STRING,
+    FunctionType,
+    List,
+    PolymorphicType,
+    PrimitiveType,
+)
+
+
+syntax = {
+    "+": FunctionType(INT, INT, INT),
+    "-": FunctionType(INT, INT, INT),
+    "head": FunctionType(List(PolymorphicType("a")), PolymorphicType("a")),
+    "non_reachable": PrimitiveType("non_reachable"),
+    "1": INT,
+    "non_productive": FunctionType(INT, STRING),
+}
+
+semantics = {"+": lambda x: lambda y: x + y, "-": lambda x: lambda y: x - y, "1": 1}
+
+type_req = FunctionType(INT, INT)
+int_lexicon = list(range(100))
+max_depth = 4
+dsl = DSL(syntax)
+validator = basic_output_validator(int_lexicon, -1)
+
+
+def test_gen() -> None:
+    samples_lexicon = [2, 3, 4]
+    pcfg = ConcretePCFG.uniform_from_cfg(ConcreteCFG.from_dsl(dsl, type_req, max_depth))
+    pcfg.init_sampling(0)
+    g = TaskGenerator(
+        LexiconSampler(int_lexicon, seed=0),
+        DSLEvaluator(semantics),
+        LexiconSampler([type_req], seed=0),
+        LexiconSampler(samples_lexicon, [0.25, 0.5, 0.25], seed=0),
+        {type_req: pcfg},
+        validator,
+    )
+    for _ in range(100):
+        task = g.generate_task()
+        assert task.type_request == type_req
+        assert len(task.specification.examples) in samples_lexicon
+        assert task.solution
+        assert task.solution.depth() <= max_depth
+        for ex in task.specification.examples:
+            assert validator(ex.output)
+            assert len(ex.inputs) == 1
+            assert all(x in int_lexicon for x in ex.inputs)
+
+
+def test_seed() -> None:
+    pcfg = ConcretePCFG.uniform_from_cfg(ConcreteCFG.from_dsl(dsl, type_req, max_depth))
+    pcfg.init_sampling(0)
+    g1 = TaskGenerator(
+        LexiconSampler(int_lexicon, seed=0),
+        DSLEvaluator(semantics),
+        LexiconSampler([type_req], seed=0),
+        LexiconSampler([2, 3, 4], [0.25, 0.5, 0.25], seed=0),
+        {type_req: pcfg},
+        validator,
+    )
+    pcfg = ConcretePCFG.uniform_from_cfg(ConcreteCFG.from_dsl(dsl, type_req, max_depth))
+    pcfg.init_sampling(0)
+    g2 = TaskGenerator(
+        LexiconSampler(int_lexicon, seed=0),
+        DSLEvaluator(semantics),
+        LexiconSampler([type_req], seed=0),
+        LexiconSampler([2, 3, 4], [0.25, 0.5, 0.25], seed=0),
+        {type_req: pcfg},
+        validator,
+    )
+    for _ in range(100):
+        assert g1.generate_task() == g2.generate_task()
