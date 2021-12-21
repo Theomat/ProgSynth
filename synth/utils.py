598f4b1067139b1a752df9c77e77219280471ab5
Theomat <theomatricon@gmail.com>
2021-12-21 16:20:25 +0100

    Added to_partial_fun utility function

--- synth/utils.py
+++ synth/utils.py
@@ -1,4 +1,5 @@
 import os
+from typing import Any, Callable, List
 
 
 def make_deterministic_hash() -> None:
@@ -8,3 +9,25 @@ def make_deterministic_hash() -> None:
     hashseed = os.getenv("PYTHONHASHSEED")
     if not hashseed:
         os.environ["PYTHONHASHSEED"] = "0"
+
+
+def to_partial_fun(f: Callable, nargs: int) -> Callable:
+    """
+    Transform a n-ary function into a function that take n arguments sequentially.
+    ```python
+    >>> def plus(a, b):
+    ...     return a + b
+    >>> to_partial_fun(plus, 2)
+    lambda a: lambda b: a + b
+    >>> # Not exactly equal to but is semantically equivalent to
+    ```
+    """
+
+    def partial(x: Any, carry_on: List) -> Any:
+        li = carry_on[:]
+        li.append(x)
+        if len(li) == nargs:
+            return f(*li)
+        return lambda y: partial(y, li)
+
+    return lambda x: partial(x, [])
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,14 @@
-from synth.utils import make_deterministic_hash
+from synth.utils import make_deterministic_hash, to_partial_fun
 
 
 def test_deterministic() -> None:
     make_deterministic_hash()
     assert hash(int) == hash(int)
+
+
+def test_to_partial_fun() -> None:
+    plus = to_partial_fun(lambda x, y: x + y, 2)
+    for a in range(10):
+        plusa = plus(a)
+        for b in range(20):
+            assert a + b == plusa(b)
