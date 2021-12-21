98f572942c8e396426027d51c966d67ba1713381
Theomat <theomatricon@gmail.com>
2021-12-21 16:00:33 +0100

    Added evaluator test

--- tests/semantic/test_evaluator.py
+++ tests/semantic/test_evaluator.py
@@ -1,1 +1,54 @@
-# TODO
+from synth.syntax.concrete.concrete_cfg import ConcreteCFG
+from synth.syntax.concrete.concrete_pcfg import ConcretePCFG
+from synth.semantic.evaluator import DSLEvaluator
+from synth.syntax.dsl import DSL
+from synth.syntax.type_system import (
+    INT,
+    STRING,
+    FunctionType,
+    List,
+    PolymorphicType,
+    PrimitiveType,
+)
+
+
+syntax = {
+    "+1": FunctionType(INT, INT),
+    "head": FunctionType(List(PolymorphicType("a")), PolymorphicType("a")),
+    "non_reachable": PrimitiveType("non_reachable"),
+    "non_productive": FunctionType(INT, STRING),
+}
+
+semantics = {
+    "+1": lambda x: x + 1,
+}
+max_depth = 4
+dsl = DSL(syntax)
+cfg = ConcreteCFG.from_dsl(dsl, FunctionType(INT, INT), max_depth)
+
+
+def test_eval() -> None:
+    eval = DSLEvaluator(semantics)
+    pcfg = ConcretePCFG.uniform_from_cfg(cfg)
+    pcfg.init_sampling(0)
+    for _ in range(100):
+        program = pcfg.sample_program()
+        try:
+            for i in range(-25, 25):
+                assert eval.eval(program, [i]) == program.length() + i - 1
+        except Exception as e:
+            assert False, e
+
+
+def test_use_cache() -> None:
+    eval = DSLEvaluator(semantics)
+    pcfg = ConcretePCFG.uniform_from_cfg(cfg)
+    pcfg.init_sampling(0)
+    for _ in range(100):
+        program = pcfg.sample_program()
+        try:
+            for i in range(-25, 25):
+                assert eval.eval(program, [i]) == program.length() + i - 1
+                assert eval._cache[tuple([i])][program] == program.length() + i - 1
+        except Exception as e:
+            assert False, e
