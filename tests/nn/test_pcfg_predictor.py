1c10409c8f3382fc7131e6f8ce1b3f98830eb0a6
Theomat <theomatricon@gmail.com>
2021-12-21 20:04:38 +0100

    Added pcfg predictor tests

---
+++ tests/nn/test_pcfg_predictor.py
@@ -0,0 +1,70 @@
+import torch
+
+import numpy as np
+from torch.functional import Tensor
+
+from synth.nn.pcfg_predictor import BigramsPredictorLayer
+from synth.syntax.concrete.concrete_cfg import ConcreteCFG
+from synth.syntax.dsl import DSL
+from synth.syntax.type_system import (
+    INT,
+    FunctionType,
+)
+
+
+syntax = {
+    "+": FunctionType(INT, INT, INT),
+    "-": FunctionType(INT, INT, INT),
+    "1": INT,
+}
+
+dsl = DSL(syntax)
+cfg = ConcreteCFG.from_dsl(dsl, INT, 4)  # FunctionType(INT, INT), 4)
+
+
+def test_forward() -> None:
+    layer = BigramsPredictorLayer(50, {cfg.type_request: cfg}, dsl)
+    generator = torch.manual_seed(0)
+    nb_parents = len(syntax) + 1 - 1
+    max_args = 2
+    for _ in range(20):
+        x = torch.randn((100, 50), generator=generator)
+        y: Tensor = layer(x)
+        assert y.shape == torch.Size([x.shape[0], nb_parents, max_args, len(syntax)])
+        ones = y.exp().sum(-1)
+        assert torch.allclose(ones, torch.ones_like(ones))
+
+
+def test_to_logpcfg() -> None:
+    layer = BigramsPredictorLayer(50, {cfg.type_request: cfg}, dsl)
+    generator = torch.manual_seed(0)
+    for _ in range(20):
+        x = torch.randn((5, 50), generator=generator)
+        y = layer(x)
+        for i in range(y.shape[0]):
+            log_pcfg = layer.tensor2pcfg(
+                y[i], cfg.type_request, total_variable_order=False
+            )
+            for S in log_pcfg.rules:
+                total = sum(
+                    np.exp(log_pcfg.rules[S][P][1].item()) for P in log_pcfg.rules[S]
+                )
+                assert np.isclose(1, total)
+
+
+def test_logpcfg2pcfg() -> None:
+    layer = BigramsPredictorLayer(50, {cfg.type_request: cfg}, dsl)
+    generator = torch.manual_seed(0)
+    for _ in range(20):
+        x = torch.randn((5, 50), generator=generator)
+        y = layer(x)
+        for i in range(y.shape[0]):
+            log_pcfg = layer.tensor2pcfg(
+                y[i], cfg.type_request, total_variable_order=False
+            )
+            pcfg = log_pcfg.to_pcfg()
+            pcfg.init_sampling(0)
+            P = pcfg.sample_program()
+            prob = pcfg.probability_program(P)
+            exp_logprob = np.exp(log_pcfg.probability_program(P).item())
+            assert np.isclose(prob, exp_logprob)
