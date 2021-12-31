82d367f9631853e115b888e53921130d83d8cdf9
Theomat <theomatricon@gmail.com>
2021-12-31 14:09:33 +0100

    Renamed and added function pruner

--- synth/pruning/__init__.py
+++ synth/pruning/__init__.py
@@ -2,4 +2,4 @@
 Module that contains anything relevant to pruning
 """
 from synth.pruning.pruner import Pruner, UnionPruner
-from synth.pruning.post_pruner import UseAllVariablesPruner
+from synth.pruning.syntaxic_pruner import UseAllVariablesPruner, FunctionPruner, SyntaxicPruner
---
+++ synth/pruning/syntaxic_pruner.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from typing import Callable, Dict, Set, Tuple
+
+from synth.pruning.pruner import Pruner
+from synth.syntax.program import Function, Primitive, Program
+from synth.syntax.type_system import Arrow, Type
+
+
+class SyntaxicPruner(Pruner, ABC):
+    @abstractmethod
+    def accept(self, obj: Tuple[Type, Program]) -> bool:
+        pass
+
+
+class UseAllVariablesPruner(SyntaxicPruner):
+    def __init__(self) -> None:
+        super().__init__()
+        self._cached_variables_set: Dict[Type, Set[int]] = {}
+
+    def __get_var_set__(self, treq: Type) -> Set[int]:
+        if treq not in self._cached_variables_set:
+            if isinstance(treq, Arrow):
+                self._cached_variables_set[treq] = set(range(len(treq.arguments())))
+            else:
+                self._cached_variables_set[treq] = set()
+
+        return self._cached_variables_set[treq]
+
+    def accept(self, obj: Tuple[Type, Program]) -> bool:
+        treq, prog = obj
+        target = self.__get_var_set__(treq)
+        return prog.used_variables() == target
+
+
+class FunctionPruner(SyntaxicPruner):
+    def __init__(self, is_useless: Dict[str, Callable]) -> None:
+        super().__init__()
+        self.is_useless = is_useless
+
+    def accept(self, obj: Tuple[Type, Program]) -> bool:
+        _, prog = obj
+        for P in prog.depth_first_iter():
+            if not isinstance(P, Function):
+                continue
+            f = P.function
+            if not isinstance(f, Primitive):
+                continue
+            if f.primitive in self.is_useless and self.is_useless[f.primitive](
+                *P.arguments
+            ):
+                return False
+        return True
