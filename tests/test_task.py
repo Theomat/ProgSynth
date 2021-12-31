23d91f2281c362f77fd9b172ee2e7758e9bd809f
Theomat <theomatricon@gmail.com>
2021-12-31 17:22:48 +0100

    Removed test_hash since it is useless

--- tests/test_task.py
+++ tests/test_task.py
@@ -61,32 +61,3 @@ def test_dataset_behaviour() -> None:
     assert dataset[-5:-1] == dataset.tasks[-5:-1]
     assert dataset.tasks == [x for x in dataset]
     assert len(dataset) == len(dataset.tasks)
-
-
-def test_dataset_hash(tmp_path: pathlib.Path) -> None:
-    file_path = tmp_path / "dataset.pickle"
-    random.seed(0)
-    dataset = Dataset(
-        [
-            Task(
-                FunctionType(INT, INT, INT),
-                PBE(
-                    [
-                        Example(
-                            [random.randint(0, 100), random.randint(0, 100)],
-                            random.randint(0, 100),
-                        )
-                        for _ in range(5)
-                    ]
-                ),
-                Variable(0, INT) if random.random() > 0.5 else None,
-                metadata={"index": i},
-            )
-            for i in range(100)
-        ],
-        metadata={"something": False, "else": "is", "coming": 42},
-    )
-    dataset.save(file_path.as_posix())
-    loaded = Dataset[PBE].load(file_path.as_posix())
-    for t1, t2 in zip(dataset, loaded):
-        assert hash(t1.type_request) == hash(t2.type_request)
