5787c9231479aefe47f3bb7809ca697b7b7bce6c
Theomat <theomatricon@gmail.com>
2022-01-05 12:17:08 +0100

    Added skip exceptions

--- synth/semantic/evaluator.py
+++ synth/semantic/evaluator.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Set
 
 from synth.syntax.program import Function, Primitive, Program, Variable
 
@@ -23,6 +23,7 @@ class DSLEvaluator(Evaluator):
         self.semantics = semantics
         self.use_cache = use_cache
         self._cache: Dict[Any, Dict[Program, Any]] = {}
+        self.skip_exceptions: Set[Exception] = set()
         # Statistics
         self._total_requests = 0
         self._cache_hits = 0
@@ -32,20 +33,29 @@ class DSLEvaluator(Evaluator):
         if key not in self._cache and self.use_cache:
             self._cache[key] = {}
         evaluations: Dict[Program, Any] = self._cache[key] if self.use_cache else {}
-        for sub_prog in program.depth_first_iter():
-            self._total_requests += 1
-            if sub_prog in evaluations:
-                self._cache_hits += 1
-                continue
-            if isinstance(sub_prog, Primitive):
-                evaluations[sub_prog] = self.semantics[sub_prog.primitive]
-            elif isinstance(sub_prog, Variable):
-                evaluations[sub_prog] = input[sub_prog.variable]
-            elif isinstance(sub_prog, Function):
-                fun = evaluations[sub_prog.function]
-                for arg in sub_prog.arguments:
-                    fun = fun(evaluations[arg])
-                evaluations[sub_prog] = fun
+        if program in evaluations:
+            return evaluations[program]
+        try:
+            for sub_prog in program.depth_first_iter():
+                self._total_requests += 1
+                if sub_prog in evaluations:
+                    self._cache_hits += 1
+                    continue
+                if isinstance(sub_prog, Primitive):
+                    evaluations[sub_prog] = self.semantics[sub_prog.primitive]
+                elif isinstance(sub_prog, Variable):
+                    evaluations[sub_prog] = input[sub_prog.variable]
+                elif isinstance(sub_prog, Function):
+                    fun = evaluations[sub_prog.function]
+                    for arg in sub_prog.arguments:
+                        fun = fun(evaluations[arg])
+                    evaluations[sub_prog] = fun
+        except Exception as e:
+            if type(e) in self.skip_exceptions:
+                evaluations[program] = None
+                return None
+            else:
+                raise e
 
         return evaluations[program]
 
