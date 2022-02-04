71b14b7d4fe241d65daccb7648067347d3e2ce39
Theomat <theomatricon@gmail.com>
2022-02-04 17:52:12 +0100

    Updated init

--- synth/nn/__init__.py
+++ synth/nn/__init__.py
@@ -1,5 +1,9 @@
 """
 Module that contains anything relevant to neural networks
 """
-from synth.nn.pcfg_predictor import BigramsPredictorLayer, loss_negative_log_prob
-from synth.nn.utils import AutoPack, Task2Tensor
+from synth.nn.pcfg_predictor import (
+    BigramsPredictorLayer,
+    loss_negative_log_prob,
+    ExactBigramsPredictorLayer,
+)
+from synth.nn.utils import AutoPack, Task2Tensor, print_model_summary
