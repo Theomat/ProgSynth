aca7219496364cfdd08f539cb7ccbe09fab2dae7
Theomat <theomatricon@gmail.com>
2022-02-04 14:15:22 +0100

    Created a dataset_generator script

---
+++ examples/pbe/dataset_generator.py
@@ -0,0 +1,90 @@
+import sys
+
+from synth import Dataset, PBE
+from synth.pbe import reproduce_dataset
+from synth.utils import chrono, gen_take
+
+DREAMCODER = "dreamcoder"
+DEEPCODER = "deepcoder"
+
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Evaluate model prediction")
+parser.add_argument(
+    "--dsl",
+    type=str,
+    default=DEEPCODER,
+    help="dsl (default: deepcoder)",
+    choices=[DEEPCODER, DREAMCODER],
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    type=str,
+    default="dataset.pickle",
+    help="output file (default: dataset.pickle)",
+)
+parser.add_argument("-s", "--seed", type=int, default=0, help="seed (default: 0)")
+parser.add_argument(
+    "--size", type=int, default=1000, help="generated dataset size (default: 1000)"
+)
+parser.add_argument(
+    "--max-depth", type=int, default=5, help="solutions max depth (default: 5)"
+)
+
+parameters = parser.parse_args()
+dsl_name: str = parameters.dsl
+output_file: str = parameters.output
+seed: int = parameters.seed
+max_depth: int = parameters.max_depth
+gen_dataset_size: int = parameters.size
+# ================================
+# Load constants specific to DSL
+# ================================
+max_list_length = None
+if dsl_name == DEEPCODER:
+    from deepcoder.deepcoder import dsl, evaluator, lexicon
+
+elif dsl_name == DREAMCODER:
+    from dreamcoder.dreamcoder import dsl, evaluator, lexicon
+
+    max_list_length = 10
+else:
+    print("Unknown dsl:", dsl_name, file=sys.stderr)
+    sys.exit(1)
+# ================================
+# Load dataset & Task Generator
+# ================================
+# Load dataset
+dataset_file = f"{dsl_name}.pickle"
+print(f"Loading {dataset_file}...", end="")
+with chrono.clock("dataset.load") as c:
+    full_dataset: Dataset[PBE] = Dataset.load(dataset_file)
+    print("done in", c.elapsed_time(), "s")
+# Reproduce dataset distribution
+print("Reproducing dataset...", end="", flush=True)
+with chrono.clock("dataset.reproduce") as c:
+    task_generator, lexicon = reproduce_dataset(
+        full_dataset,
+        dsl,
+        evaluator,
+        seed,
+        max_list_length=max_list_length,
+        default_max_depth=max_depth,
+    )
+    print("done in", c.elapsed_time(), "s")
+# Add some exceptions that are ignored during task generation
+task_generator.skip_exceptions.add(TypeError)
+with chrono.clock("dataset.generate") as c:
+    gen_dataset = Dataset(gen_take(task_generator.generator(), gen_dataset_size))
+with chrono.clock("dataset.save") as c:
+    gen_dataset.save("./train_dataset.pickle")
+
+# ================================
+# Print some stats
+# ================================
+# Generate the CFG dictionnary
+all_type_requests = gen_dataset.type_requests()
+print(f"{len(all_type_requests)} type requests supported.")
+print(f"Lexicon: [{min(lexicon)};{max(lexicon)}]")
