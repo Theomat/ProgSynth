e4d569974e381afe3fe0063147a9775070cecf33
Theomat <theomatricon@gmail.com>
2022-07-04 21:58:43 +0200

    Fixed formatting

--- synth/tools/type_constraints/__init__.py
+++ synth/tools/type_constraints/__init__.py
@@ -1,1 +1,3 @@
-from synth.tools.type_constraints.pattern_constraints import produce_new_syntax_for_constraints
+from synth.tools.type_constraints.pattern_constraints import (
+    produce_new_syntax_for_constraints,
+)
