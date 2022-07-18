1f713d518ed66ced3df9aeb387a9710b86e8278a
Theomat <theomatricon@gmail.com>
2022-07-18 18:25:57 +0200

    Added option to display a progress bar

--- synth/utils/generator_utils.py
+++ synth/utils/generator_utils.py
@@ -1,15 +1,17 @@
 from typing import Any, Generator, List, TypeVar
 
+from tqdm import trange
 T = TypeVar("T")
 
 
-def gen_take(gen: Generator[T, Any, Any], n: int) -> List[T]:
+def gen_take(gen: Generator[T, Any, Any], n: int, progress: bool = False) -> List[T]:
     """
     Take the first n elements of a generator and return them as a list.
     """
     out: List[T] = []
     try:
-        for _ in range(n):
+        ite = range(n) if not progress else trange(n)
+        for _ in ite:
             out.append(next(gen))
     except StopIteration:
         pass
