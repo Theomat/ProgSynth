16461519a34630d9ea67491ebbef3811d814003b
Theomat <theomatricon@gmail.com>
2022-07-29 17:54:43 +0200

    Moved folders

--- examples/pbe/transduction/knowledge_graph/lists_query.py
+++ examples/pbe/transduction/knowledge_graph/lists_query.py
@@ -1,14 +1,14 @@
-from typing import List, Set, Tuple
+from typing import List, Tuple
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 SPARQL_ENDPOINT = "http://192.168.1.20:9999/blazegraph/namespace/kb/sparql"
 
 
-def make_query_path(distance: int, id: int, tabs: int = 1) -> str:
+def __make_query_path__(distance: int, id: int, tabs: int = 1) -> str:
     if distance == 0:
-        return ("\t" * tabs) + f"yago:{{}} ?p{distance} ?o_{id}_{distance} ."
+        return ("\t" * tabs) + f"w:{{}} ?p{distance} ?o_{id}_{distance} ."
     else:
-        path = make_query_path(distance - 1, id, tabs) + "\n"
+        path = __make_query_path__(distance - 1, id, tabs) + "\n"
         path += (
             "\t" * tabs
         ) + f"?o_{id}_{distance-1} ?p{distance} ?o_{id}_{distance} ."
@@ -21,19 +21,19 @@ def build_query(entities: List[Tuple[str, str]], distance: int = 1) -> str:
     for i, item in enumerate(entities):
         subquery += "\tFILTER EXISTS {\n"
         subquery += (
-            make_query_path(distance, i + 1, 2)
-            .format(item[0])
-            .replace(f"?o_{i + 1}_{distance}", "yago:" + item[1])
+            __make_query_path__(distance, i + 1, 2)
+            .format(item[1])
+            .replace(f"?o_{i + 1}_{distance}", "w:" + item[0])
         )
         subquery += "\n\t} .\n"
-    sparql_request = "PREFIX yago: <http://yago-knowledge.org/resource/>\n"
+    sparql_request = "PREFIX w: <https://en.wikipedia.org/wiki/>\n"
     sparql_request += "SELECT "
     sparql_request += " ".join(f"?p{d}" for d in range(distance + 1))
     sparql_request += " WHERE {\n"
     sparql_request += (
-        make_query_path(distance, 0)
-        .format(first[0])
-        .replace(f"?o_{0}_{distance}", "yago:" + first[1])
+        __make_query_path__(distance, 0)
+        .format(first[1])
+        .replace(f"?o_{0}_{distance}", "w:" + first[0])
     )
     sparql_request += "\n"
     sparql_request += subquery
@@ -41,19 +41,26 @@ def build_query(entities: List[Tuple[str, str]], distance: int = 1) -> str:
     return sparql_request
 
 
-def execute_query(query: str, endpoint: str = SPARQL_ENDPOINT) -> Set[str]:
+def execute_query(query: str, endpoint: str = SPARQL_ENDPOINT) -> List[List[str]]:
     wrapper = SPARQLWrapper(endpoint)
     wrapper.setReturnFormat(JSON)
     wrapper.setQuery(query)
     answer = wrapper.query().convert()
-    properties: Set[str] = set()
-    for r in answer["results"]["bindings"]:
-        properties.add("yago:" + r["obj"]["value"].split("/")[-1])
-    return properties
+    paths: List[List[str]] = []
+    for path in answer["results"]["bindings"]:
+        cur_path = []
+        for rel in path:
+            cur_path.append(path[rel]["value"].split("/")[-1])
+        paths.append(cur_path)
+    return paths
 
 
 if __name__ == "__main__":
-    countries = [("France", "Paris"), ("Poland", "Warsaw"), ("Belgium", "Brussels")]
+    countries = [
+        ("France", "Paris"),
+        ("United_States", "Washington"),
+        ("Germany", "Berlin"),
+    ]
     query = build_query(countries, 2)
     print(query)
     print(execute_query(query))
