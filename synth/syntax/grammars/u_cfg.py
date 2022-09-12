be5580c633c4a4a3a1c7b9ae641a85827326dc2a
Theomat <theomatricon@gmail.com>
2022-09-12 11:31:05 +0200

    Added depth_constraint

--- synth/syntax/grammars/u_cfg.py
+++ synth/syntax/grammars/u_cfg.py
@@ -1,12 +1,15 @@
 from typing import (
+    Dict,
     List,
+    Set,
     Tuple,
     TypeVar,
     Generic,
     Union,
 )
+from synth.syntax.dsl import DSL
 
-from synth.syntax.grammars.cfg import NoneType
+from synth.syntax.grammars.cfg import CFG, CFGState, NoneType
 from synth.syntax.grammars.u_grammar import UGrammar
 from synth.syntax.program import Constant, Primitive, Variable
 from synth.syntax.type_system import Type
@@ -47,3 +50,47 @@ class UCFG(UGrammar[U, List[Tuple[Type, U]], NoneType], Generic[U]):
         """
         possibles = self.rules[S][P]
         return [(None, possible[0], possible) for possible in possibles]
+
+    @classmethod
+    def depth_constraint(
+        cls,
+        dsl: DSL,
+        type_request: Type,
+        max_depth: int,
+        upper_bound_type_size: int = 10,
+        min_variable_depth: int = 1,
+        n_gram: int = 2,
+        recursive: bool = False,
+        constant_types: Set[Type] = set(),
+    ) -> "UCFG[CFGState]":
+        """
+        Constructs a CFG from a DSL imposing bounds on size of the types
+        and on the maximum program depth.
+
+        max_depth: int - is the maxium depth of programs allowed
+        uppder_bound_size_type: int - is the maximum size type allowed for polymorphic type instanciations
+        min_variable_depth: int - min depth at which variables and constants are allowed
+        n_gram: int - the context, a bigram depends only in the parent node
+        recursvie: bool - allows the generated programs to call themselves
+        constant_types: Set[Type] - the set of of types allowed for constant objects
+        """
+        cfg = CFG.depth_constraint(
+            dsl,
+            type_request,
+            max_depth,
+            upper_bound_type_size,
+            min_variable_depth,
+            n_gram,
+            recursive,
+            constant_types,
+        )
+        rules: Dict[
+            Tuple[Type, CFGState],
+            Dict[DerivableProgram, List[List[Tuple[Type, CFGState]]]],
+        ] = {}
+        for S in cfg.rules:
+            nS = (S[0], S[1][0])
+            rules[nS] = {}
+            for P in cfg.rules[S]:
+                rules[nS][P] = [[SS for SS in cfg.rules[S][P][0]]]
+        return UCFG((cfg.start[0], cfg.start[1][0]), rules)
