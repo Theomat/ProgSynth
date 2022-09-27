7df6ed592338eef9a3d10c7c0f161d1c03c643b6
Theomat <theomatricon@gmail.com>
2022-09-27 10:45:54 +0200

    Improved tests

--- tests/syntax/grammars/test_ucfg.py
+++ tests/syntax/grammars/test_ucfg.py
@@ -1,3 +1,4 @@
+from synth.syntax.grammars.cfg import CFG
 from synth.syntax.grammars.u_cfg import UCFG
 from synth.syntax.dsl import DSL
 from synth.syntax.program import Primitive
@@ -10,6 +11,8 @@ from synth.syntax.type_system import (
     PrimitiveType,
 )
 
+import pytest
+
 
 syntax = {
     "+": FunctionType(INT, INT, INT),
@@ -18,32 +21,39 @@ syntax = {
     "1": INT,
     "non_productive": FunctionType(INT, STRING),
 }
+dsl = DSL(syntax)
+max_depths = [3, 7, 11]
 
 
-def test_clean() -> None:
-    dsl = DSL(syntax)
-    for max_depth in [3, 7, 11]:
-        cfg = UCFG.depth_constraint(dsl, FunctionType(INT, INT), max_depth)
-        for rule in cfg.rules:
-            assert rule[1][1] <= max_depth
-            for P in cfg.rules[rule]:
-                if isinstance(P, Primitive):
-                    assert P.primitive != "non_reachable"
-                    assert P.primitive != "non_productive"
-                    assert P.primitive != "head"
+@pytest.mark.parametrize("max_depth", max_depths)
+def test_size(max_depth: int) -> None:
+    ucfg = UCFG.depth_constraint(dsl, FunctionType(INT, INT), max_depth)
+    cfg = CFG.depth_constraint(dsl, FunctionType(INT, INT), max_depth)
+    assert cfg.programs() == ucfg.programs()
 
 
-def test_depth_constraint() -> None:
-    dsl = DSL(syntax)
-    for max_depth in [3, 7, 11]:
-        cfg = UCFG.depth_constraint(dsl, FunctionType(INT, INT), max_depth)
-        res = dsl.parse_program("(+ 1 var0)", FunctionType(INT, INT))
-        print(cfg)
-        while res.depth() <= max_depth:
-            assert (
-                res in cfg
-            ), f"Program depth:{res.depth()} should be in the TTCFG max_depth:{max_depth}"
-            res = dsl.parse_program(f"(+ {res} var0)", FunctionType(INT, INT))
+@pytest.mark.parametrize("max_depth", max_depths)
+def test_clean(max_depth: int) -> None:
+    cfg = UCFG.depth_constraint(dsl, FunctionType(INT, INT), max_depth)
+    for rule in cfg.rules:
+        assert rule[1][1] <= max_depth
+        for P in cfg.rules[rule]:
+            if isinstance(P, Primitive):
+                assert P.primitive != "non_reachable"
+                assert P.primitive != "non_productive"
+                assert P.primitive != "head"
+
+
+@pytest.mark.parametrize("max_depth", max_depths)
+def test_depth_constraint(max_depth: int) -> None:
+    cfg = UCFG.depth_constraint(dsl, FunctionType(INT, INT), max_depth)
+    res = dsl.parse_program("(+ 1 var0)", FunctionType(INT, INT))
+    print(cfg)
+    while res.depth() <= max_depth:
         assert (
-            res not in cfg
-        ), f"Program depth:{res.depth()} should NOT be in the TTCFG max_depth:{max_depth}"
+            res in cfg
+        ), f"Program depth:{res.depth()} should be in the TTCFG max_depth:{max_depth}"
+        res = dsl.parse_program(f"(+ {res} var0)", FunctionType(INT, INT))
+    assert (
+        res not in cfg
+    ), f"Program depth:{res.depth()} should NOT be in the TTCFG max_depth:{max_depth}"
