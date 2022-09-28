ff080f806012730293356d5a131672f82bf43202
Theomat <theomatricon@gmail.com>
2022-09-28 16:27:18 +0200

    Added tests for u_heap_search

---
+++ tests/syntax/grammars/enumeration/test_u_heap_search.py
@@ -0,0 +1,81 @@
+from synth.syntax.grammars.enumeration.u_heap_search import (
+    Bucket,
+    enumerate_prob_u_grammar,
+    enumerate_bucket_prob_u_grammar,
+)
+from synth.syntax.grammars.u_cfg import UCFG
+from synth.syntax.grammars.tagged_u_grammar import ProbUGrammar
+from synth.syntax.dsl import DSL
+from synth.syntax.type_system import (
+    INT,
+    STRING,
+    FunctionType,
+    List,
+    PolymorphicType,
+    PrimitiveType,
+)
+
+import pytest
+
+
+syntax = {
+    "+": FunctionType(INT, INT, INT),
+    "head": FunctionType(List(PolymorphicType("a")), PolymorphicType("a")),
+    "non_reachable": PrimitiveType("non_reachable"),
+    "1": INT,
+    "non_productive": FunctionType(INT, STRING),
+}
+dsl = DSL(syntax)
+dsl.instantiate_polymorphic_types()
+testdata = [
+    UCFG.depth_constraint(dsl, FunctionType(INT, INT), 5),
+]
+
+
+@pytest.mark.parametrize("cfg", testdata)
+def test_unicity_heapSearch(cfg: UCFG) -> None:
+    pcfg = ProbUGrammar.uniform(cfg)
+    seen = set()
+    print(cfg)
+    print("programs:", cfg.programs())
+    for program in enumerate_prob_u_grammar(pcfg):
+        assert program not in seen
+        seen.add(program)
+    assert len(seen) == cfg.programs()
+
+
+@pytest.mark.parametrize("cfg", testdata)
+def test_order_heapSearch(cfg: UCFG) -> None:
+    pcfg = ProbUGrammar.uniform(cfg)
+    last = 1.0
+    for program in enumerate_prob_u_grammar(pcfg):
+        p = pcfg.probability(program)
+        assert p <= last
+        last = p
+
+
+@pytest.mark.parametrize("cfg", testdata)
+def test_unicity_bucketSearch(cfg: UCFG) -> None:
+    pcfg = ProbUGrammar.uniform(cfg)
+    for bucketSize in range(3, 10):
+        seen = set()
+        for program in enumerate_bucket_prob_u_grammar(pcfg, bucket_size=bucketSize):
+            assert program not in seen
+            seen.add(program)
+        assert len(seen) == cfg.programs()
+
+
+@pytest.mark.parametrize("cfg", testdata)
+def test_order_bucketSearch(cfg: UCFG) -> None:
+    pcfg = ProbUGrammar.uniform(cfg)
+    for bucketSize in range(3, 10):
+        last = Bucket(bucketSize)
+        for program in enumerate_bucket_prob_u_grammar(pcfg, bucket_size=bucketSize):
+            p = pcfg.reduce_derivations(
+                lambda b, S, P, _: b.add_prob_uniform(pcfg.probabilities[S][P]),
+                Bucket(bucketSize),
+                program,
+            )
+            assert p.size == bucketSize
+            assert p >= last or last == Bucket(bucketSize)
+            last = p
