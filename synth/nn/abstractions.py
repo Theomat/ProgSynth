f2979b0eefa82cc22094bc579cae779bb22ee5bd
Theomat <theomatricon@gmail.com>
2022-10-04 09:24:03 +0200

    Added ttcfg abstraction

--- synth/nn/abstractions.py
+++ synth/nn/abstractions.py
@@ -1,8 +1,27 @@
-from typing import Any, Optional, Tuple
+from typing import Any, Optional, Tuple, TypeVar
 from synth.syntax.grammars.cfg import CFGState, NoneType
 from synth.syntax.grammars.det_grammar import DerivableProgram
+from synth.syntax.grammars.grammar import NGram
 from synth.syntax.program import Primitive
 from synth.syntax.type_system import Type
+
+T = TypeVar("T")
+S = TypeVar("S")
+
+
+def ttcfg_bigram(
+    ctx: Tuple[Type, Tuple[S, T]]
+) -> Optional[Tuple[DerivableProgram, int]]:
+    """
+    Abstract away a TTCFG into tuples of (parent, no_arg).
+    We lose any other information.
+    """
+    _, (ngram, __) = ctx
+    while not isinstance(ngram, NGram):
+        ngram = ngram[0]  # type: ignore
+    if len(ngram) > 0:
+        return ngram.last()
+    return None
 
 
 def cfg_bigram_without_depth(
