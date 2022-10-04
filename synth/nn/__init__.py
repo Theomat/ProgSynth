49dec91ecb9300b85f257096927d78ebfcc2e1e7
Theomat <theomatricon@gmail.com>
2022-10-04 10:46:45 +0200

    WIP U Grammar Predictor Layer

--- synth/nn/__init__.py
+++ synth/nn/__init__.py
@@ -2,6 +2,7 @@
 Module that contains anything relevant to neural networks
 """
 from synth.nn.det_grammar_predictor import DetGrammarPredictorLayer
+from synth.nn.u_grammar_predictor import UGrammarPredictorLayer
 import synth.nn.abstractions as abstractions
 from synth.nn.utils import (
     AutoPack,
