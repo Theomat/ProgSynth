3036ab19400cb6d272faf32a67ff74c74004ddf6
Theomat <theomatricon@gmail.com>
2022-10-05 09:23:59 +0200

    Updated experiment script

--- experiments/dreamcoder/evaluate_models.sh
+++ experiments/dreamcoder/evaluate_models.sh
@@ -17,6 +17,7 @@ DREAMCODER_DATASET="./dreamcoder.pickle"
 TEST_DATASET=$DREAMCODER_DATASET
 TRAIN_DATASET="$EXPERIMENT_FOLDER/train.pickle"
 MODEL_CFG_FILE="$EXPERIMENT_FOLDER/model_cfg.pt"
+MODEL_UCFG_FILE="$EXPERIMENT_FOLDER/model_ucfg.pt"
 # ===================================================================
 # MAIN CODE
 # ===================================================================
@@ -30,7 +31,7 @@ mkdir -p $EXPERIMENT_FOLDER
 # Make train dataset
 if [  ! -f "$TRAIN_DATASET" ]; then
     echo "[Generation] Creating the train dataset."
-    python examples/pbe/dataset_generator.py --dsl dreamcoder --dataset $TEST_DATASET --seed $SEED --size $TRAIN_SIZE -o $TRAIN_DATASET --uniform
+    python examples/pbe/dataset_generator.py --dsl dreamcoder --dataset $TEST_DATASET --seed $SEED --size $TRAIN_SIZE -o $TRAIN_DATASET --uniform --constrained
     if [ $? != "0" ]; then
         exit 1
     fi
@@ -48,6 +49,20 @@ echo "[Evaluation] Evaluating the model from the raw DSL."
 python examples/pbe/evaluate.py --dsl dreamcoder --dataset $TEST_DATASET --b $BATCH_SIZE --model $MODEL_CFG_FILE -o $EXPERIMENT_FOLDER -t $TIMEOUT
 if [ $? != "0" ]; then
     exit 3
+fi
+# Train cfg model
+if [  ! -f "$MODEL_UCFG_FILE" ]; then
+    echo "[Training] Creating the CFG model."
+    python examples/pbe/model_trainer.py --dsl dreamcoder --dataset $TRAIN_DATASET --seed $SEED --b $BATCH_SIZE -o $MODEL_UCFG_FILE -e $EPOCHS --constrained
+    if [ $? != "0" ]; then
+        exit 4
+    fi
+fi
+# Evaluate cfg model
+echo "[Evaluation] Evaluating the model from the raw DSL."
+python examples/pbe/evaluate.py --dsl dreamcoder --dataset $TEST_DATASET --b $BATCH_SIZE --model $MODEL_UCFG_FILE -o $EXPERIMENT_FOLDER -t $TIMEOUT --constrained
+if [ $? != "0" ]; then
+    exit 5
 fi
 # Plotting
 python examples/plot_results.py --dataset $TEST_DATASET --folder $EXPERIMENT_FOLDER
