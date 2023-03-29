f8debcf93e579d2939046aa54c80191a29889c9e
Theomat <theomatricon@gmail.com>
2023-03-29 17:24:01 +0200

    Working gen. algo

--- examples/pbe/dataset_generator_separated.py
+++ examples/pbe/dataset_generator_separated.py
@@ -1,12 +1,13 @@
 import argparse
 from collections import defaultdict
-from typing import Any, Callable, Tuple, List
+from typing import Any, Callable, Dict, Tuple, List
 from dsl_loader import add_dsl_choice_arg, load_DSL
 import tqdm
 
 from synth import Dataset, PBE
 from synth.pbe.task_generator import TaskGenerator
 from synth.syntax.program import Program
+from synth.syntax.type_system import Type
 from synth.utils import chrono
 from synth.syntax import CFG
 
@@ -132,6 +133,59 @@ task_generator.skip_exceptions.add(TypeError)
 task_generator.uniques = not no_unique
 task_generator.verbose = True
 
+
+def generate_programs_and_samples_for(
+    tr: Type, nb_programs: int, task_generator: TaskGenerator
+):
+
+    # 3 Phases algorithm to generate m programs
+    # Phase 1 generate n programs
+    # Phase 2 generate k examples to differentiate as much as possible programs in n
+    # Phase 3 try to generate new programs to get n as close as possible to m
+    # Phase 1
+    programs = []
+    for _ in tqdm.trange(nb_programs * 2, desc="1: generation"):
+        prog, unique = task_generator.generate_program(tr)
+        while not no_unique and not unique:
+            prog, unique = task_generator.generate_program(tr)
+        programs.append(prog)
+    # Phase 2
+    samples, equiv = generate_samples_for(
+        programs,
+        lambda: task_generator.sample_input(tr.arguments()),
+        task_generator.eval_input,
+        task_generator.evaluator.clear_cache,
+    )
+    # Phase 3
+    pbar = tqdm.tqdm(total=nb_programs - len(equiv), desc="3: improvement")
+    tries = 0
+    while len(equiv) < nb_programs:
+        prog, unique = task_generator.generate_program(tr)
+        while not no_unique and not unique:
+            prog, unique = task_generator.generate_program(tr)
+        # Compute semantic hash
+        cl = None
+        for x in samples:
+            o = task_generator.eval_input(prog, x)
+            if isinstance(o, List):
+                o = tuple(o)
+            cl = (o, cl)
+        # Check
+        if cl not in equiv:
+            equiv[cl].append(prog)
+            tries = 0
+            pbar.update(1)
+        else:
+            tries += 1
+            if tries > 1000:
+                break
+
+    programs = [
+        min([(p.length(), p) for p in v], key=lambda x: x[0])[1] for v in equiv.values()
+    ]
+    return samples, programs
+
+
 #
 def generate_samples_for(
     programs: List[Program],
@@ -140,22 +194,18 @@ def generate_samples_for(
     clear_cache: Callable,
     threshold: int = 1000,
     examples: int = 5,
-) -> Tuple[List, List[Program]]:
+) -> Tuple[List, Dict[Any, List[Program]]]:
     samples = []
     equiv_classes = {None: programs}
-    i = 1
-    n = 0
-    pbar = tqdm.tqdm(total=examples - 1)
-    # Nice try
+    nb_examples = 0
+    nb_tested = 0
+    pbar = tqdm.tqdm(total=examples * threshold, desc="2: sem. unicity")
     best = None
     best_score = 0
-    while 2 * len(equiv_classes) < len(programs) and i < examples:
+    while nb_examples < examples:
         next_equiv_classes = defaultdict(list)
         clear_cache()
-        if n > threshold:
-            ui = best
-        else:
-            ui = input_sampler()
+        ui = best if nb_tested > threshold else input_sampler()
         for cl, prog in equiv_classes.items():
             for p in prog:
                 o = eval_prog(p, ui)
@@ -166,86 +216,107 @@ def generate_samples_for(
         if len(next_equiv_classes) > best_score:
             best = ui
             best_score = len(next_equiv_classes)
+        # Early stopping if no new examples is interesting
+        if nb_tested > threshold and best_score == len(equiv_classes):
+            break
+        # If timeout or good example
         if (
-            n > threshold
-            or len(next_equiv_classes) * (ratio ** (examples - i)) >= len(programs) / 2
+            nb_tested > threshold
+            or len(next_equiv_classes) * (ratio ** (examples - nb_examples))
+            >= len(programs) / 2
         ):
-            i += 1
+            nb_examples += 1
+            nb_tested = 0
             pbar.update(1)
             equiv_classes = next_equiv_classes
             samples.append(ui)
-            n = 0
             best_score = len(next_equiv_classes)
-        else:
             pbar.set_postfix_str(
-                f"{n} tested/ {len(next_equiv_classes)} from {len(equiv_classes)}({len(programs)})"
+                f"{len(equiv_classes)}->{best_score} | {best_score/len(programs):.0%}"
             )
-        n += 1
-        # pbar.set_postfix_str(f"{n} tested")
-    # Brute try
+            pbar.n = nb_examples * threshold
+        nb_tested += 1
+        pbar.update(1)
+    clear_cache()
     pbar.close()
-    to_keep = [
-        min([(p.length(), p) for p in v], key=lambda x: x[0])[1]
-        for v in equiv_classes.values()
-    ]
-    return samples, to_keep
+    return samples, equiv_classes
 
 
 #
-
-print("Generating programs...", nb_programs, end="", flush=True)
-type_requests = set()
-with chrono.clock("dataset.generate.programs") as c:
-    programs = []
-    programs_by_tr = defaultdict(list)
-    assert isinstance(task_generator, TaskGenerator)
-    for i in tqdm.trange(2 * nb_programs, desc="programs generated"):
+print("Computing task type distribution...", end="", flush=True)
+programs_by_tr = defaultdict(int)
+with chrono.clock("dataset.generate.distribution") as c:
+    for i in tqdm.trange(nb_programs):
         tr = task_generator.generate_type_request()
-        prog, unique = task_generator.generate_program(tr)
-        while not no_unique and not unique:
-            tr = task_generator.generate_type_request()
-            prob, unique = task_generator.generate_program(tr)
-        type_requests.add(tr)
-        programs_by_tr[tr].append(prog)
-        programs.append((tr, prog))
+        programs_by_tr[tr] += 1
     print("done in", c.elapsed_time(), "s")
-print("Generating inputs...", nb_inputs, end="", flush=True)
+tasks = []
 
-filtered_programs = []
-with chrono.clock("dataset.generate.inputs") as c:
-    inputs = {}
-    for tr in type_requests:
-        inputs[tr] = []
-        args = tr.arguments()
-        assert isinstance(task_generator, TaskGenerator)
-        for i in tqdm.trange(nb_inputs, desc=f"inputs generated for {tr}"):
-            sample, to_keep = generate_samples_for(
-                programs_by_tr[tr],
-                lambda: task_generator.sample_input(args),
-                lambda p, x: task_generator.eval_input(p, x),
-                task_generator.evaluator.clear_cache,
+print("Generating tasks by type...", flush=True)
+for tr, count in programs_by_tr.items():
+    print("\t", tr)
+    samples, programs = generate_programs_and_samples_for(tr, count, task_generator)
+    for program in programs:
+        tasks.append(
+            task_generator.make_task(
+                tr,
+                program,
+                samples,
+                [task_generator.eval_input(program, x) for x in samples],
             )
-            inputs[tr].append(sample)
-            for p in to_keep:
-                filtered_programs.append((tr, p))
-    print("done in", c.elapsed_time(), "s")
-    print("Kept", len(filtered_programs), "/", len(programs), "programs")
+        )
 
-print("Evaluating inputs...", end="", flush=True)
-with chrono.clock("dataset.evaluation.inputs") as c:
-    tasks = []
-    for tr, program in tqdm.tqdm(filtered_programs, desc="programs evaluated"):
-        for sample in inputs[tr]:
-            tasks.append(
-                task_generator.make_task(
-                    tr,
-                    program,
-                    sample,
-                    [task_generator.eval_input(program, x) for x in sample],
-                )
-            )
 
-    print("done in", c.elapsed_time(), "s")
+# type_requests = set()
+# with chrono.clock("dataset.generate.programs") as c:
+#     programs_by_tr = defaultdict(list)
+#     assert isinstance(task_generator, TaskGenerator)
+#     for i in tqdm.trange(nb_gen_programs, desc="programs generated"):
+#         tr = task_generator.generate_type_request()
+#         prog, unique = task_generator.generate_program(tr)
+#         while not no_unique and not unique:
+#             tr = task_generator.generate_type_request()
+#             prog, unique = task_generator.generate_program(tr)
+#         type_requests.add(tr)
+#         programs_by_tr[tr].append(prog)
+#     print("done in", c.elapsed_time(), "s")
+# print("Generating inputs...", nb_inputs, end="", flush=True)
+
+# filtered_programs = []
+# with chrono.clock("dataset.generate.inputs") as c:
+#     inputs = {}
+#     for tr in type_requests:
+#         inputs[tr] = []
+#         args = tr.arguments()
+#         assert isinstance(task_generator, TaskGenerator)
+#         for i in tqdm.trange(nb_inputs, desc=f"inputs generated for {tr}"):
+#             sample, to_keep = generate_samples_for(
+#                 programs_by_tr[tr],
+#                 lambda: task_generator.sample_input(args),
+#                 lambda p, x: task_generator.eval_input(p, x),
+#                 task_generator.evaluator.clear_cache,
+#             )
+#             inputs[tr].append(sample)
+#             for p in to_keep:
+#                 filtered_programs.append((tr, p))
+#     print("done in", c.elapsed_time(), "s")
+#     print("Kept", len(filtered_programs), "/", nb_gen_programs, "programs")
+
+# print("Evaluating inputs...", end="", flush=True)
+# with chrono.clock("dataset.evaluation.inputs") as c:
+#     tasks = []
+#     for tr, program in tqdm.tqdm(filtered_programs, desc="programs evaluated"):
+#         for sample in inputs[tr]:
+#             tasks.append(
+#                 task_generator.make_task(
+#                     tr,
+#                     program,
+#                     sample,
+#                     [task_generator.eval_input(program, x) for x in sample],
+#                 )
+#             )
+
+#     print("done in", c.elapsed_time(), "s")
 gen_dataset = Dataset(tasks)
 print("Saving dataset...", end="", flush=True)
 with chrono.clock("dataset.save") as c:
