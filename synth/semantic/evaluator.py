98fba2dfe301ccf8199b1220dc5a30f6fbfa4ca6
Theomat <theomatricon@gmail.com>
2023-04-13 14:34:15 +0200

    Removed unused code

--- synth/semantic/evaluator.py
+++ synth/semantic/evaluator.py
@@ -25,24 +25,6 @@ def __tuplify__(element: Any) -> Any:
         return element
 
 
-def auto_complete_semantics(
-    primitives: Iterable[str], semantics: Dict[str, Any]
-) -> None:
-    """
-    Copy the semantics for all primitives that are not semantically defined yet there are defined up to prefix before @.
-    Examples:
-        1) and, and@0, and@1
-        Defining only and and then autocompleting will give the same semantic to the 3 previous primitives
-        2) or@0
-        Since or is not defined semantically then or@0 is not either.
-    """
-    for prim in primitives:
-        if "@" in prim and prim not in semantics:
-            prefix = prim[: prim.index("@")]
-            if prefix in semantics:
-                semantics[prim] = semantics[prefix]
-
-
 class DSLEvaluator(Evaluator):
     def __init__(self, semantics: Dict[str, Any], use_cache: bool = True) -> None:
         super().__init__()
