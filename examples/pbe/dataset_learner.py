234229de00666d70f1eea942c9c04471821d5971
Theomat <theomatricon@gmail.com>
2023-06-06 07:44:55 +0200

    Update learning script according to changes

--- examples/pbe/dataset_learner.py
+++ examples/pbe/dataset_learner.py
@@ -1,6 +1,6 @@
 from synth import Dataset, PBE
 from synth.utils import chrono
-from synth.library import learn
+from synth.library import learn, make_score_probabilistic, score_description
 
 from dsl_loader import add_dsl_choice_arg, load_DSL
 
@@ -14,9 +14,14 @@ parser.add_argument(
     default="{dsl_name}.pickle",
     help="dataset file (default: {dsl_name}.pickle)",
 )
-
+parser.add_argument(
+    "--probabilistic",
+    action="store_true",
+    help="Maximise probability instead of reducing description size",
+)
 parameters = parser.parse_args()
 dsl_name: str = parameters.dsl
+proba: bool = parameters.probabilistic
 dataset_file: str = parameters.dataset.format(dsl_name=dsl_name)
 # ================================
 # Load constants specific to DSL
@@ -32,8 +37,18 @@ with chrono.clock("dataset.load") as c:
     full_dataset: Dataset[PBE] = Dataset.load(dataset_file)
     print("done in", c.elapsed_time(), "s")
 
-size, occs, prog = learn(
-    [t.solution for t in full_dataset if t.solution is not None], progress=True
-)
-print(f"Found {occs} occurences of {prog}.")
-print(f"This would reduce the size by {(size - 1) * occs}.")
+programs = [t.solution for t in full_dataset if t.solution is not None]
+score_fn = make_score_probabilistic(programs, False) if proba else score_description
+score, prog = learn(programs, score_fn, progress=True)
+if proba:
+    print(
+        f"Best program is {prog} which bumps the programs set's log prob to:{score}."
+    )
+else:
+    print(f"Best program is {prog} which reduces description size by:{score}.")
+# print(f"This would reduce the size by {(size - 1) * occs}.")
+# score, prog = learn(
+#     [t.solution for t in full_dataset if t.solution is not None], progress=True
+# )
+# # print(f"Found {occs} occurences of {prog}.")
+# print(f"This would reduce the size by {score}.")
