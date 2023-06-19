6856132bdc0889ae0c7164e9bbfc7b2b57f33cbc
Theomat <theomatricon@gmail.com>
2023-06-19 14:08:03 +0200

    Implemented dataset loader

---
+++ examples/pbe/dataset_loader.py
@@ -0,0 +1,35 @@
+from argparse import ArgumentParser
+import _pickle as cPickle  # type: ignore
+
+from colorama import Fore as F
+
+from synth import Dataset
+from synth.utils import chrono
+
+
+class DatasetUnpickler(cPickle.Unpickler):
+    def find_class(self, module, name):
+        try:
+            return super().find_class(module, name)
+        except:
+            return super().find_class(module + "." + module, name)
+
+
+def add_dataset_choice_arg(parser: ArgumentParser) -> None:
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="{dsl_name}.pickle",
+        help="the dataset file to load (default: {dsl_name}.pickle)",
+    )
+
+
+def load_dataset(dsl_name: str, dataset_file: str, verbose: bool = True) -> Dataset:
+    dataset_file = dataset_file.format(dsl_name=dsl_name)
+    if verbose:
+        print(f"Loading {F.LIGHTCYAN_EX}{dataset_file}{F.RESET}...", end="")
+    with chrono.clock("dataset.load") as c:
+        full_dataset: Dataset = Dataset.load(dataset_file, DatasetUnpickler)
+        if verbose:
+            print(f"done in {c.elapsed_time():.2}s")
+        return full_dataset
