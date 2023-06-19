9c48bce5cd3cf6d795da99ca6a0d92cc9547ec88
Theomat <theomatricon@gmail.com>
2023-06-19 15:31:25 +0200

    Implemented save and load object

--- synth/utils/__init__.py
+++ synth/utils/__init__.py
@@ -3,3 +3,5 @@ Utility objects and functions that do not fit elsewhere.
 """
 import synth.utils.chrono as chrono
 from synth.utils.generator_utils import gen_take
+from synth.utils.data_storage import load_object, save_object
+
---
+++ synth/utils/data_storage.py
@@ -0,0 +1,28 @@
+import bz2
+import pickle
+from typing import Any, Callable, Optional
+import pickletools
+
+import _pickle as cPickle  # type: ignore
+
+
+def load_object(
+    path: str, unpickler: Optional[Callable[[bz2.BZ2File], pickle.Unpickler]] = None
+) -> Any:
+    """
+    Load an arbitrary object from the specified file.
+    """
+    with bz2.BZ2File(path, "rb") as fd:
+        unp = cPickle if unpickler is None else unpickler(fd)
+        return unp.load()
+
+
+def save_object(path: str, obj: Any, optimize: bool = True) -> None:
+    """
+    Save an arbitrary object to the specified path.
+    """
+    with bz2.BZ2File(path, "w") as fd:
+        content = pickle.dumps(obj)
+        if optimize:
+            content = pickletools.optimize(content)
+        fd.write(content)
