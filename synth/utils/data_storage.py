df38c0e368a104b76b9635577583b6fdf747bfaf
Theomat <theomatricon@gmail.com>
2023-07-21 13:49:42 +0200

    Added optional parameters compression level

--- synth/utils/data_storage.py
+++ synth/utils/data_storage.py
@@ -17,11 +17,14 @@ def load_object(
             return unpickler(fd).load()
 
 
-def save_object(path: str, obj: Any, optimize: bool = True) -> None:
+def save_object(
+    path: str, obj: Any, optimize: bool = True, compress_level: int = 9
+) -> None:
     """
     Save an arbitrary object to the specified path.
+    Compression level must be in 1-9 where 9 is the highest level.
     """
-    with bz2.BZ2File(path, "w") as fd:
+    with bz2.BZ2File(path, "w", compresslevel=compress_level) as fd:
         content = pickle.dumps(obj)
         if optimize:
             content = pickletools.optimize(content)
