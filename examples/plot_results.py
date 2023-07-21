9b9d5a75f19acf94c325e76e81c2a7a7bfd91a32
Theomat <theomatricon@gmail.com>
2023-07-21 14:37:52 +0200

    Added more plotting options

--- examples/plot_results.py
+++ examples/plot_results.py
@@ -7,10 +7,9 @@ import pltpublish as pub
 import csv
 
 
-def load_data(no_sort: bool):
-    max_time, max_programs = 0, 0
-    max_tasks = 0
-
+def load_data(
+    dataset_name: str, output_folder: str, verbose: bool = False
+) -> Dict[str, Dict[int, List]]:
     # Dict[name, Dict[seed, data]]
     methods = {}
 
@@ -44,30 +43,23 @@ def load_data(no_sort: bool):
             # Pop columns names
             trace.pop(0)
             # Type conversion (success, time, num_of_programs)
-            trace = [(row[0] == "True", float(row[1]), int(row[2])) for row in trace]
+            trace = [
+                (int(row[0] == "True"), float(row[1]), int(row[2])) for row in trace
+            ]
         if len(trace) == 0:
             if verbose:
                 print(f"\tskipped: no data")
             continue
-        # Compute data for first plot
-        trace_time = trace if no_sort else sorted(trace, key=lambda x: x[1])
-        cum_sol1, cum_time = np.cumsum([row[0] for row in trace_time]), np.cumsum(
-            [row[1] for row in trace_time]
-        )
-        # Compute data for second plot
-        trace_programs = trace if no_sort else sorted(trace, key=lambda x: x[2])
-        cum_sol2, cum_programs = np.cumsum(
-            [row[0] for row in trace_programs]
-        ), np.cumsum([row[2] for row in trace_programs])
         # Save data for method
-        methods[name][seed] = (cum_sol1, cum_time, cum_sol2, cum_programs)
-        # Update maxis
-        max_tasks = max(len(trace), max_tasks)
-        max_time = max(max_time, cum_time[-1])
-        max_programs = max(max_programs, cum_programs[-1])
-
-        print(f"{name} (seed={seed}) solved", cum_sol2[-1], "/", len(trace))
-    return methods, max_tasks, max_programs, max_time
+        methods[name][seed] = trace
+        if verbose:
+            print(
+                f"{name} (seed={seed}) solved",
+                sum(x[0] for x in trace),
+                "/",
+                len(trace),
+            )
+    return methods
 
 
 def plot_with_incertitude(
@@ -99,64 +91,76 @@ def plot_with_incertitude(
     ax.fill_between(target_x, mean - std, mean + std, color=color, alpha=0.5)
 
 
-def plot_tasks_wrt_time(
-    ax: plt.Axes, methods: Dict[str, Dict[int, List[float]]]
-) -> None:
-    # Plot data with incertitude
-    for method, seeds_dico in methods.items():
-        seeds = list(seeds_dico.keys())
-        plot_with_incertitude(
-            ax,
-            [seeds_dico[seed][1] for seed in seeds],
-            [seeds_dico[seed][0] for seed in seeds],
-            method.capitalize(),
-        )
-    ax.set_xlabel("Time (in s)")
-    ax.set_ylabel("Tasks Completed")
-    ax.grid()
-    ax.hlines(
-        [max_tasks],
-        xmin=0,
-        xmax=max_time,
-        label="All tasks",
-        color="k",
-        linestyles="dashed",
-    )
-    ax.set_xlim(0, max_time)
-    ax.set_ylim(0, max_tasks + 10)
-
-
-def plot_tasks_wrt_programs(
-    ax: plt.Axes, methods: Dict[str, Dict[int, List[float]]]
-) -> None:
-    # Plot data with incertitude
-    for method, seeds_dico in methods.items():
-        seeds = list(seeds_dico.keys())
-        plot_with_incertitude(
-            ax,
-            [seeds_dico[seed][3] for seed in seeds],
-            [seeds_dico[seed][2] for seed in seeds],
-            method.capitalize(),
-        )
-    ax.set_xlabel("Programs enumerated")
-    ax.set_ylabel("Tasks Completed")
-    ax.grid()
-    ax.hlines(
-        [max_tasks],
-        xmin=0,
-        xmax=max_programs,
-        label="All tasks",
-        color="k",
-        linestyles="dashed",
-    )
-    ax.set_xlim(0, max_programs)
-    ax.set_ylim(0, max_tasks + 10)
-
-
-__PLOTS__ = {
-    "tasks_wrt_time": plot_tasks_wrt_time,
-    "tasks_wrt_programs": plot_tasks_wrt_programs,
+__DATA__ = {
+    "tasks": (0, "Tasks completed", 10, True),
+    "time": (1, "Time (in s)", 5, False),
+    "programs": (2, "Programs Enumerated", 0, False),
 }
+
+
+def plot_y_wrt_x(
+    ax: plt.Axes,
+    methods: Dict[str, Dict[int, List]],
+    should_sort: bool,
+    x_name: str,
+    y_name: str,
+) -> None:
+    # Plot data with incertitude
+    a_index, a_name, a_margin, show_len_a = __DATA__[y_name]
+    b_index, b_name, b_margin, show_len_b = __DATA__[x_name]
+    max_a = 0
+    max_b = 0
+    data_length = 0
+    for method, seeds_dico in methods.items():
+        seeds = list(seeds_dico.keys())
+        data = [
+            [(elems[b_index], elems[a_index]) for elems in seeds_dico[seed]]
+            for seed in seeds
+        ]
+        data_length = max(data_length, len(data[0]))
+        if should_sort:
+            data = [sorted(seed_data) for seed_data in data]
+        xdata = [np.cumsum([x[0] for x in seed_data]) for seed_data in data]
+        ydata = [np.cumsum([x[1] for x in seed_data]) for seed_data in data]
+        max_a = max(np.max(ydata), max_a)
+        max_b = max(np.max(xdata), max_b)
+        plot_with_incertitude(
+            ax,
+            xdata,
+            ydata,
+            method.capitalize(),
+        )
+    ax.set_xlabel(b_name)
+    ax.set_ylabel(a_name)
+    ax.grid()
+    if show_len_a:
+        ax.hlines(
+            [data_length],
+            xmin=0,
+            xmax=max_b + b_margin,
+            label=f"All {y_name}",
+            color="k",
+            linestyles="dashed",
+        )
+        max_a = data_length
+    ax.set_xlim(0, max_b + b_margin)
+    ax.set_ylim(0, max_a + a_margin)
+    ax.legend()
+
+
+def make_plot_builder_for(x_name: str, y_name: str) -> None:
+    def f(ax: plt.Axes, methods: Dict[str, Dict[int, List]], should_sort: bool) -> None:
+        return plot_y_wrt_x(ax, methods, should_sort, x_name, y_name)
+
+    return f
+
+
+__PLOTS__ = {}
+for ydata in list(__DATA__.keys()):
+    for xdata in list(__DATA__.keys()):
+        if xdata == ydata:
+            continue
+        __PLOTS__[f"{ydata}_wrt_{xdata}"] = make_plot_builder_for(xdata, ydata)
 
 if __name__ == "__main__":
     import argparse
@@ -202,11 +206,8 @@ if __name__ == "__main__":
     dataset_name = dataset_file[start_index : dataset_file.index(".", start_index)]
     # Load data
     pub.setup()
-    methods, max_tasks, max_programs, max_time = load_data(no_sort)
+    methods = load_data(dataset_name, output_folder, verbose)
 
-    if verbose:
-        print(f"Max Time: {max_time}s")
-        print(f"Max Programs: {max_programs}")
     # Check we have at least one file
     if len(methods) == 0:
         import sys
@@ -217,6 +218,6 @@ if __name__ == "__main__":
     # Plotting
     for count, to_plot in enumerate(plots):
         ax = plt.subplot(1, len(plots), count + 1)
-        __PLOTS__[to_plot](ax, methods)
+        __PLOTS__[to_plot](ax, methods, not no_sort)
 
     plt.show()
