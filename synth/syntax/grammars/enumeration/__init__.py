d330a824eade8aa5e7b9dc0e2972fc2e07d5226c
Theomat <theomatricon@gmail.com>
2023-12-03 12:50:12 +0100

    Fixed incorrect import

--- synth/syntax/grammars/enumeration/__init__.py
+++ synth/syntax/grammars/enumeration/__init__.py
@@ -13,6 +13,6 @@ from synth.syntax.grammars.enumeration.bee_search import (
     enumerate_prob_grammar as bs_enumerate_prob_grammar,
 )
 
-from synth.syntax.grammars.enumeration.bee_search import (
+from synth.syntax.grammars.enumeration.beep_search import (
     enumerate_prob_grammar as bps_enumerate_prob_grammar,
 )
