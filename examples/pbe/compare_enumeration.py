354f0fb9a2aa0ce3f940e4b1a7bcfd2e7d447c87
Theomat <theomatricon@gmail.com>
2023-12-11 11:28:39 +0100

    Added script to compare enumeration

---
+++ examples/pbe/compare_enumeration.py
@@ -0,0 +1,141 @@
+from typing import Callable, Iterable, List, Tuple, Union
+import csv
+import time
+
+from dsl_loader import add_dsl_choice_arg, load_DSL
+
+
+from synth.syntax import (
+    ProbDetGrammar,
+    ProbUGrammar,
+    CFG,
+    DSL,
+    hs_enumerate_prob_grammar,
+    bs_enumerate_prob_grammar,
+    bpluss_enumerate_prob_grammar,
+    bps_enumerate_prob_grammar,
+    hs_enumerate_prob_u_grammar,
+    ProgramEnumerator,
+    Type,
+)
+
+import tqdm
+
+import argparse
+
+from synth.syntax.type_helper import auto_type
+
+
+SEARCH_ALGOS = {
+    "beep_search": (bps_enumerate_prob_grammar, None),
+    "heap_search": (hs_enumerate_prob_grammar, hs_enumerate_prob_u_grammar),
+    # "bee_search": (bs_enumerate_prob_grammar, None),
+    "bee+_search": (bpluss_enumerate_prob_grammar, None),
+}
+
+parser = argparse.ArgumentParser(
+    description="Compare search algorithms", fromfile_prefix_chars="@"
+)
+add_dsl_choice_arg(parser)
+parser.add_argument(
+    "-o",
+    "--output",
+    type=str,
+    default="./enumeration.csv",
+    help="output file (default: './enumeration.csv')",
+)
+parser.add_argument(
+    dest="n", metavar="programs", type=int, help="number of programs to be enumerated"
+)
+parser.add_argument("type", type=str, help="type request")
+parser.add_argument(
+    "-d", "--depth", type=int, default=5, help="max program depth (default: 5)"
+)
+parser.add_argument("-s", "--seed", type=int, default=0, help="seed (default: 0)")
+
+
+parameters = parser.parse_args()
+dsl_name: str = parameters.dsl
+output_file: str = parameters.output
+str_tr: str = parameters.type
+programs: int = parameters.n
+max_depth: int = parameters.depth
+seed: int = parameters.seed
+
+file_name = output_file[: -len(".csv")]
+suffix = f"_dsl_{dsl_name}_seed_{seed}_depth_{max_depth}"
+if not file_name.endswith(suffix):
+    file_name += suffix
+output_file = file_name + ".csv"
+
+
+# ================================
+# Load constants specific to dataset
+# ================================
+
+
+def save(trace: Iterable) -> None:
+    with open(output_file, "w") as fd:
+        writer = csv.writer(fd)
+        writer.writerows(trace)
+
+
+# Enumeration methods =====================================================
+def enumerative_search(
+    cfgs: List[CFG],
+    name: str,
+    custom_enumerate: Callable[
+        [Union[ProbDetGrammar, ProbUGrammar]], ProgramEnumerator
+    ],
+    programs: int,
+    datum_each: int = 1000,
+) -> List[Tuple[str, Type, float, int, int, int]]:
+    out = []
+    for cfg in tqdm.tqdm(cfgs, desc=name):
+        n = 0
+        start = time.perf_counter()
+        enumerator = custom_enumerate(cfg)
+        for program in enumerator.generator():
+            n += 1
+            if n % datum_each == 0 or n >= programs:
+                out.append(
+                    (
+                        name,
+                        cfg.type_request,
+                        time.perf_counter() - start,
+                        n,
+                        enumerator.programs_in_queues(),
+                        enumerator.programs_in_banks(),
+                    )
+                )
+                if n >= programs:
+                    break
+    return out
+
+
+# Main ====================================================================
+
+if __name__ == "__main__":
+    dsl_module = load_DSL(dsl_name)
+    dsl: DSL = dsl_module.dsl
+
+    base_types = set()
+    for p in dsl.list_primitives:
+        base_types |= p.type.decompose_type()[0]
+
+    type_requests = [auto_type(str_tr)]
+    pcfgs = []
+    for tr in type_requests:
+        try:
+            cfg = CFG.depth_constraint(dsl, tr, max_depth)
+            pcfg = ProbDetGrammar.random(cfg, seed=seed)
+            pcfgs.append(pcfg)
+        except KeyError:
+            pass
+    assert len(pcfgs) > 0, f"failed to generate any simple CFGs for {type_requests}"
+    print(f"generated {len(pcfgs)} CFGs")
+    trace = [("search", "type", "time", "programs", "queue", "bank")]
+    for name, (enum, _) in SEARCH_ALGOS.items():
+        trace += enumerative_search(pcfgs, name, enum, programs)
+    save(trace)
+    print("csv file was saved as:", output_file)
---
+++ examples/plot_enumeration_results.py
@@ -0,0 +1,145 @@
+from glob import glob
+import os
+from collections import OrderedDict, defaultdict
+from typing import Dict, List
+import matplotlib.pyplot as plt
+import pltpublish as pub
+import csv
+
+from plot_helper import (
+    plot_y_wrt_x,
+    make_plot_wrapper,
+    plot_dist,
+    plot_rank_by,
+)
+
+
+__DATA__ = {
+    "time": (0, "Time (in s)", 5, False, False),
+    "programs": (1, "Programs Enumerated", 0, False, False),
+    "queued": (2, "Programs Enqueued", 0, False, False),
+    "banked": (3, "Programs in Banks", 0, False, False),
+}
+
+
+def load_data(output_folder: str, verbose: bool = False) -> Dict[str, Dict[int, List]]:
+    # Dict[name, Dict[seed, data]]
+    methods = {}
+
+    chosen_dsl = None
+
+    for file in glob(os.path.join(output_folder, "*.csv")):
+        filename = os.path.relpath(file, output_folder)
+        if verbose:
+            print("found csv file:", file)
+        # filename should end with dsl_{dsl}_seed_{seed}_depth_{max_depth}.csv
+        name = filename[:-4]
+        if "_seed_" not in name:
+            if verbose:
+                print(f"\tskipped: does not contain _seed_")
+            continue
+
+        dsl = name[name.index("dsl_") + 4 : name.index("_seed_")].replace("_", " ")
+        if not (chosen_dsl is None or dsl == chosen_dsl):
+            if verbose:
+                print(
+                    f"\tskipped: does not contain data belonging to the same dsl: {dsl}"
+                )
+                continue
+            if chosen_dsl is None:
+                chosen_dsl = dsl
+                if verbose:
+                    print(f"found dsl: {dsl}")
+        seed = int(name[name.index("_seed_") + 6 : name.index("_depth_")])
+
+        trace = []
+        with open(os.path.join(output_folder, filename), "r") as fd:
+            reader = csv.reader(fd)
+            trace = [tuple(row) for row in reader]
+            # Pop columns names
+            columns = {name: ind for ind, name in enumerate(trace.pop(0))}
+            indices = [
+                columns["search"],
+                columns["time"],
+                columns["programs"],
+                columns["queue"],
+                columns["bank"],
+            ]
+            data = [tuple(row[k] if k >= 0 else 0 for k in indices) for row in trace]
+            if len(data) == 0:
+                if verbose:
+                    print(f"\tskipped: no data")
+                continue
+            agg = defaultdict(list)
+            for row in data:
+                agg[row[0]].append(row[1:])
+
+            for name, data in agg.items():
+                if name not in methods:
+                    methods[name] = {}
+                if seed in methods[name]:
+                    print(f"Warning: duplicate seed {seed} for method {name}!")
+                    continue
+
+                # Save data for method
+                methods[name][seed] = [tuple(float(x) for x in row) for row in data]
+    return methods
+
+
+# Generate all possible combinations
+__PLOTS__ = {}
+for ydata in list(__DATA__.keys()):
+    for xdata in list(__DATA__.keys()):
+        if xdata == ydata:
+            continue
+        __PLOTS__[f"{ydata}_wrt_{xdata}"] = make_plot_wrapper(
+            plot_y_wrt_x, __DATA__[xdata], __DATA__[ydata]
+        )
+    if ydata != "tasks":
+        __PLOTS__[f"rank_by_{ydata}"] = make_plot_wrapper(plot_rank_by, __DATA__[ydata])
+        __PLOTS__[f"dist_{ydata}_by_programs"] = make_plot_wrapper(
+            plot_dist, __DATA__[ydata], "tasks"
+        )
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser(description="Plot results")
+
+    parser.add_argument(
+        "--folder",
+        type=str,
+        default="./",
+        help="folder in which to look for CSV files (default: './')",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="verbose mode",
+    )
+    parser.add_argument("plots", nargs="+", choices=list(__PLOTS__.keys()))
+    parameters = parser.parse_args()
+    output_folder: str = parameters.folder
+    verbose: bool = parameters.verbose
+    plots: List[str] = parameters.plots
+
+    # Load data
+    pub.setup()
+    methods = load_data(output_folder, verbose)
+    # Check we have at least one file
+    if len(methods) == 0:
+        print("Error: no performance file was found!", file=sys.stderr)
+        sys.exit(1)
+    # Order by name so that it is always the same color for the same methods if diff. DSL
+    ordered_methods = OrderedDict()
+    for met in sorted(methods.keys()):
+        ordered_methods[met] = methods[met]
+    # Plotting
+    for count, to_plot in enumerate(plots):
+        ax = plt.subplot(1, len(plots), count + 1)
+        __PLOTS__[to_plot](ax, ordered_methods)
+    plt.show()
