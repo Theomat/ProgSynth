62e7e4d6868e3ba538c0b914a8df01232800c293
Theomat <theomatricon@gmail.com>
2023-12-11 10:58:11 +0100

    refactoring

--- examples/plot_solve_results.py
+++ examples/plot_solve_results.py
@@ -1,11 +1,19 @@
 from glob import glob
 import os
 from collections import OrderedDict
-from typing import Dict, List, Tuple, Optional
+from typing import Dict, List, Tuple
 import numpy as np
 import matplotlib.pyplot as plt
 import pltpublish as pub
 import csv
+
+
+from plot_helper import (
+    plot_y_wrt_x,
+    make_plot_wrapper,
+    plot_dist,
+    plot_rank_by,
+)
 
 
 def load_data(
@@ -166,45 +174,6 @@ def filter(
     }
 
 
-def plot_with_incertitude(
-    ax: plt.Axes,
-    x: List[np.ndarray],
-    y: List[np.ndarray],
-    label: str,
-    std_factor: float = 1.96,
-    miny: Optional[float] = None,
-    maxy: Optional[float] = None,
-) -> None:
-    max_len = max(len(xi) for xi in x)
-    x = [xi for xi in x if len(xi) == max_len]
-    y = [yi for yi in y if len(yi) == max_len]
-
-    x_min = np.min(np.array(x))
-    x_max = np.max(np.array(x))
-    if x_max == x_min:
-        return
-    target_x = np.arange(x_min, x_max + 1, step=(x_max - x_min) / 50)
-    # Interpolate data
-    data = []
-    for xi, yi in zip(x, y):
-        nyi = np.interp(target_x, xi, yi)
-        data.append(nyi)
-    # Compute distribution
-    Y = np.array(data)
-    mean = np.mean(Y, axis=0)
-    std = std_factor * np.std(Y, axis=0)
-
-    p = ax.plot(target_x, mean, label=label)
-    color = p[0].get_color()
-    upper = mean + std
-    if maxy is not None:
-        upper = np.minimum(upper, maxy)
-    lower = mean - std
-    if miny is not None:
-        lower = np.maximum(lower, miny)
-    ax.fill_between(target_x, lower, upper, color=color, alpha=0.5)
-
-
 __DATA__ = {
     "tasks": (0, "Tasks completed", 10, True, True),
     "time": (1, "Time (in s)", 5, False, False),
@@ -214,203 +183,6 @@ __DATA__ = {
 }
 
 
-def plot_y_wrt_x(
-    ax: plt.Axes,
-    methods: Dict[str, Dict[int, List]],
-    should_sort: bool,
-    x_name: str,
-    y_name: str,
-) -> None:
-    # Plot data with incertitude
-    a_index, a_name, a_margin, show_len_a, _ = __DATA__[y_name]
-    b_index, b_name, b_margin, show_len_b, _ = __DATA__[x_name]
-    max_a = 0
-    max_b = 0
-    data_length = 0
-    for method, seeds_dico in methods.items():
-        seeds = list(seeds_dico.keys())
-        data = [
-            [(elems[b_index], elems[a_index]) for elems in seeds_dico[seed]]
-            for seed in seeds
-        ]
-        data_length = max(data_length, len(data[0]))
-        if should_sort:
-            data = [sorted(seed_data) for seed_data in data]
-        xdata = [np.cumsum([x[0] for x in seed_data]) for seed_data in data]
-        ydata = [np.cumsum([x[1] for x in seed_data]) for seed_data in data]
-        max_a = max(max(np.max(yi) for yi in ydata), max_a)
-        max_b = max(max(np.max(xi) for xi in xdata), max_b)
-        plot_with_incertitude(
-            ax,
-            xdata,
-            ydata,
-            method.capitalize(),
-            maxy=data_length if show_len_a else None,
-        )
-    ax.set_xlabel(b_name)
-    ax.set_ylabel(a_name)
-    ax.grid()
-    if show_len_a:
-        ax.hlines(
-            [data_length],
-            xmin=0,
-            xmax=max_b + b_margin,
-            label=f"All {y_name}",
-            color="k",
-            linestyles="dashed",
-        )
-        max_a = data_length
-    ax.set_xlim(0, max_b + b_margin)
-    ax.set_ylim(0, max_a + a_margin)
-    ax.legend()
-
-
-def make_plot_wrapper(func, *args) -> None:
-    def f(ax: plt.Axes, methods: Dict[str, Dict[int, List]], should_sort: bool) -> None:
-        return func(ax, methods, should_sort, *args)
-
-    return f
-
-
-def get_rank_matrix(
-    methods: Dict[str, Dict[int, List]], yindex: int, maximize: bool
-) -> Tuple[List[str], np.ndarray]:
-    seeds = list(methods.values())[0].keys()
-    task_len = len(list(list(methods.values())[0].values())[0])
-    rank_matrix = np.ndarray((len(methods), task_len, len(methods)), dtype=float)
-    method_names = list(methods.keys())
-    data = np.ndarray((len(methods), len(seeds)), dtype=float)
-    np.random.seed(1)
-    for task_no in range(task_len):
-        for i, method in enumerate(method_names):
-            for j, seed in enumerate(seeds):
-                data[i, j] = methods[method][seed][task_no][yindex]
-        # data_for_seed = []
-        # for method in method_names:
-        #     data = methods[method][seed]
-        #     data_for_seed.append([d[yindex] for d in data])
-        # data_for_seed = np.array(data_for_seed)
-        if maximize:
-            data = -data
-        rand_x = np.random.random(size=data.shape)
-        # This is done to randomly break ties.
-        # Last key is the primary key,
-        indices = np.lexsort((rand_x, data), axis=0)
-        for i, method in enumerate(method_names):
-            rank_matrix[i, task_no] = [
-                np.sum(indices[i] == rank) / len(seeds) for rank in range(len(methods))
-            ]
-    return rank_matrix
-
-
-def __ready_for_stacked_dist_plot__(ax: plt.Axes) -> None:
-    ax.spines["top"].set_visible(False)
-    ax.spines["right"].set_visible(False)
-    ax.spines["bottom"].set_visible(False)
-    ax.spines["left"].set_visible(False)
-    ax.tick_params(
-        axis="both",
-        which="both",
-        bottom=False,
-        top=False,
-        left=True,
-        right=False,
-        labeltop=False,
-        labelbottom=True,
-        labelleft=True,
-        labelright=False,
-    )
-
-    ax.legend(fancybox=True, fontsize="large")
-
-
-def plot_rank_by(
-    ax: plt.Axes, methods: Dict[str, Dict[int, List]], should_sort: bool, y_name: str
-) -> None:
-    width = 1.0
-    a_index, a_name, a_margin, show_len_a, should_max = __DATA__[y_name]
-    rank_matrix = get_rank_matrix(methods, a_index, should_max)
-    labels = list(range(1, len(methods) + 1))
-    mean_ranks = np.mean(rank_matrix, axis=-2)
-    bottom = np.zeros_like(mean_ranks[0])
-    for i, key in enumerate(methods.keys()):
-        label = key
-        bars = ax.bar(
-            labels,
-            mean_ranks[i],
-            width,
-            label=label,
-            bottom=bottom,
-            alpha=0.9,
-            linewidth=1,
-            edgecolor="white",
-        )
-        ax.bar_label(bars, labels=[f"{x:.1%}" for x in mean_ranks[i]])
-        bottom += mean_ranks[i]
-
-    ax.set_ylabel("Fraction (in %)", size="large")
-    yticks = np.array(range(0, 101, 20))
-    ax.set_yticklabels(yticks)
-    ax.set_yticks(yticks * 0.01)
-    ax.set_xlabel("Ranking", size="large")
-    ax.set_xticks(labels)
-    ax.set_xticklabels(labels)
-    __ready_for_stacked_dist_plot__(ax)
-    word = "Most" if should_max else "Least"
-    ax.set_title(f"{word} {a_name}")
-
-
-def plot_dist(
-    ax: plt.Axes, methods: Dict[str, Dict[int, List]], should_sort: bool, y_name: str
-) -> None:
-    width = 1.0
-    data_length = 0
-    a_index, a_name, a_margin, show_len_a, should_max = __DATA__[y_name]
-    max_a = max(
-        max(max([y[a_index] for y in x]) for x in seed_dico.values())
-        for seed_dico in methods.values()
-    )
-    bottom = None
-    nbins = 5
-    bins = [max_a]
-    while len(bins) <= nbins:
-        bins.insert(0, np.sqrt(bins[0] + 1))
-    for i in range(nbins):
-        if bins[i + 1] < 2 * bins[i]:
-            bins[i + 1] = 2 * bins[i]
-    x_bar = list(range(1, nbins + 1))
-    for method, seeds_dico in methods.items():
-        hists = []
-        for seed, raw_data in seeds_dico.items():
-            data = [x[a_index] for x in raw_data]
-            data_length = max(data_length, len(data))
-            hist, edges = np.histogram(
-                data, bins=bins, range=(1e-3, max_a), density=False
-            )
-            hists.append(hist)
-        true_hist = np.mean(hists, axis=0) / data_length
-        if bottom is None:
-            bottom = np.zeros_like(true_hist)
-        label = method
-        bars = ax.bar(
-            x_bar,
-            true_hist,
-            width,
-            label=label,
-            bottom=bottom,
-            alpha=0.9,
-            linewidth=1,
-            edgecolor="white",
-        )
-        ax.bar_label(bars, labels=[f"{x:.1%}" for x in true_hist])
-        bottom += true_hist
-    __ready_for_stacked_dist_plot__(ax)
-    ax.set_yticklabels([])
-    ax.set_xlabel(a_name, size="large")
-    ax.set_xticklabels(map(lambda x: f"<{x:.0f}", edges))
-    ax.set_title(f"Distribution of {a_name} per task")
-
-
 # Generate all possible combinations
 __PLOTS__ = {}
 for ydata in list(__DATA__.keys()):
@@ -418,11 +190,13 @@ for ydata in list(__DATA__.keys()):
         if xdata == ydata:
             continue
         __PLOTS__[f"{ydata}_wrt_{xdata}"] = make_plot_wrapper(
-            plot_y_wrt_x, xdata, ydata
+            plot_y_wrt_x, __DATA__[xdata], __DATA__[ydata]
         )
     if ydata != "tasks":
-        __PLOTS__[f"rank_by_{ydata}"] = make_plot_wrapper(plot_rank_by, ydata)
-        __PLOTS__[f"dist_{ydata}_by_task"] = make_plot_wrapper(plot_dist, ydata)
+        __PLOTS__[f"rank_by_{ydata}"] = make_plot_wrapper(plot_rank_by, __DATA__[ydata])
+        __PLOTS__[f"dist_{ydata}_by_task"] = make_plot_wrapper(
+            plot_dist, __DATA__[ydata], "tasks"
+        )
 
 
 if __name__ == "__main__":
@@ -498,5 +272,5 @@ if __name__ == "__main__":
     # Plotting
     for count, to_plot in enumerate(plots):
         ax = plt.subplot(1, len(plots), count + 1)
-        __PLOTS__[to_plot](ax, ordered_methods, not no_sort)
+        __PLOTS__[to_plot](ax, ordered_methods)
     plt.show()
