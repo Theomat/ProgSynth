bb90b6b81e0f8ba3a9d46647911597d2aaaed9d1
Theomat <theomatricon@gmail.com>
2024-01-11 08:32:24 +0100

    Implemented DFTAPruner

--- synth/pruning/__init__.py
+++ synth/pruning/__init__.py
@@ -1,7 +1,8 @@
 """
 Module that contains anything relevant to pruning
 """
-from synth.pruning.pruner import Pruner, UnionPruner
+from synth.pruning.pruner import Pruner, UnionPruner, IntersectionPruner
+from synth.pruning.dfta_pruner import DFTAPruner
 from synth.pruning.syntactic_pruner import (
     UseAllVariablesPruner,
     FunctionPruner,
---
+++ synth/pruning/dfta_pruner.py
@@ -0,0 +1,39 @@
+from typing import Dict, Generic, TypeVar, Optional
+
+from synth.pruning.pruner import Pruner
+from synth.syntax.automata.tree_automaton import DFTA
+from synth.syntax.grammars.grammar import DerivableProgram
+from synth.syntax.program import Function, Program, Lambda
+
+V = TypeVar("V")
+
+
+class DFTAPruner(Pruner, Generic[V]):
+    def __init__(self, dfta: DFTA[V, DerivableProgram]) -> None:
+        self.dfta = dfta
+        self._cache: Dict[Program, V] = {}
+
+    def _get_prog_state(self, prog: Program) -> Optional[V]:
+        state = self._cache.get(prog, None)
+        if state is not None:
+            return state
+        if isinstance(prog, Function):
+            fun = prog.function
+            args = tuple(self._get_prog_state(arg) for arg in prog.arguments)
+            state = self.dfta.read(fun, args)  # type: ignore
+            if state is not None:
+                self._cache[prog] = state
+            return state
+        elif isinstance(prog, Lambda):
+            assert False, "Not implemented"
+        else:
+            state = self.dfta.read(prog, ())  # type: ignore
+            if state is not None:
+                self._cache[prog] = state
+            return state
+
+    def accept(self, obj: Program) -> bool:
+        return self._get_prog_state(obj) is not None
+
+    def reset_cache(self) -> None:
+        self._cache.clear()
