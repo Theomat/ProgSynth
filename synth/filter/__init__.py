1c289e0388e5e60db6d1bb3c0851304c360a4fbc
Theomat <theomatricon@gmail.com>
2024-02-09 10:23:41 +0100

    Added LocalStatelessFilter to init

--- synth/filter/__init__.py
+++ synth/filter/__init__.py
@@ -4,6 +4,7 @@ Module that contains anything relevant to pruning
 from synth.filter.filter import Filter, UnionFilter, IntersectionFilter
 from synth.filter.dfta_filter import DFTAFilter
 from synth.filter.obs_eq_filter import ObsEqFilter
+from synth.filter.local_stateless_filter import LocalStatelessFilter
 from synth.filter.syntactic_filter import (
     UseAllVariablesFilter,
     FunctionFilter,
