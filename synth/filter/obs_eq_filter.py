4212ff879375d1c567e5f36fb2dcf9bfd8ce1323
Theomat <theomatricon@gmail.com>
2024-02-27 11:01:41 +0100

    Bug fix

--- synth/filter/obs_eq_filter.py
+++ synth/filter/obs_eq_filter.py
@@ -1,15 +1,17 @@
+from collections import defaultdict
 from typing import Any, Dict, List, Tuple
 
 from synth.filter.filter import Filter
 from synth.semantic.evaluator import Evaluator
 from synth.syntax.program import Program
+from synth.syntax.type_system import Type
 
 
 class ObsEqFilter(Filter):
     def __init__(self, evaluator: Evaluator, inputs_list: List[List[Any]]) -> None:
         self.evaluator = evaluator
         self.inputs_list = inputs_list
-        self._cache: Dict[Tuple[Any, ...], Program] = {}
+        self._cache: Dict[Type, Dict[Tuple[Any, ...], Program]] = defaultdict(dict)
 
     def _eval(self, prog: Program) -> bool:
         """
@@ -21,11 +23,11 @@ class ObsEqFilter(Filter):
             if out is None:
                 return False
             outputs = (outputs, out)
-        original = self._cache.get(outputs)  # type: ignore
-        if original is not None:
+        original = self._cache[prog.type].get(outputs)  # type: ignore
+        if original is not None and hash(original) != hash(prog):
             return False
         else:
-            self._cache[outputs] = prog  # type: ignore
+            self._cache[prog.type][outputs] = prog  # type: ignore
             return True
 
     def accept(self, obj: Program) -> bool:
