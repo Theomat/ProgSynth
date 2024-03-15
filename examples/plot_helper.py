fd037e8b35f01d729a9b52018ecc1fc06f8e124e
Theomat <theomatricon@gmail.com>
2024-03-15 14:56:44 +0100

    Fixed incorrect end point in arange

--- examples/plot_helper.py
+++ examples/plot_helper.py
@@ -28,8 +28,9 @@ def plot_with_incertitude(
         x_mean = np.cumsum(np.mean(X, axis=0))
         x_min = np.min(x_mean)
         x_max = np.max(x_mean)
+        step = (x_max - x_min) / n_points
         target_x = (
-            np.arange(x_min, x_max, step=(x_max - x_min) / n_points)
+            np.arange(x_min, x_max + step / 2, step=step)
             if len(X.shape) > 1 or len(X) > n_points
             else X.reshape(-1)
         )
@@ -41,8 +42,9 @@ def plot_with_incertitude(
         y_var = np.interp(target_x, x_mean, y_var)
         std = std_factor * np.sqrt(y_var)
     else:
+        step = (x_max - x_min) / n_points
         target_x = (
-            np.arange(x_min, x_max, step=(x_max - x_min) / n_points)
+            np.arange(x_min, x_max + step / 2, step=step)
             if len(X.shape) > 1 or len(X) > n_points
             else X.reshape(-1)
         )
