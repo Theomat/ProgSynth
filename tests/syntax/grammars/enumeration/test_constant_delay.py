71132b92d562823141e7b332707e78b01bf8ccdd
Theomat <theomatricon@gmail.com>
2024-06-11 10:44:53 +0200

    formatting

--- tests/syntax/grammars/enumeration/test_constant_delay.py
+++ tests/syntax/grammars/enumeration/test_constant_delay.py
@@ -1,7 +1,9 @@
 from synth.syntax.grammars.enumeration.constant_delay import (
     enumerate_prob_grammar,
 )
-from synth.syntax.grammars.enumeration.beap_search import enumerate_prob_grammar as enumerate
+from synth.syntax.grammars.enumeration.beap_search import (
+    enumerate_prob_grammar as enumerate,
+)
 
 from synth.syntax.grammars.cfg import CFG
 from synth.syntax.grammars.ttcfg import TTCFG
@@ -38,20 +40,25 @@ testdata = [
 kvals = [4, 16, 64]
 precision = [1e-3, 1e-5, 1e-8]
 
-@pytest.mark.parametrize("cfg", testdata)
-@pytest.mark.parametrize("k", kvals)
-@pytest.mark.parametrize("precis", precision)
-def test_equality_beep_search(cfg: TTCFG, k : int, precis: float) -> None:
-    pcfg = ProbDetGrammar.random(cfg, seed=1)
-    g1 = enumerate(pcfg)
-    g2 = enumerate_prob_grammar(pcfg, k, precis)
-    for p1, p2 in zip(g1, g2):
-        assert abs(pcfg.probability(p1) - pcfg.probability(p2)) <= 1e-8 or abs(pcfg.probability(p1) / pcfg.probability(p2)) <= 1 + precis
 
 @pytest.mark.parametrize("cfg", testdata)
 @pytest.mark.parametrize("k", kvals)
 @pytest.mark.parametrize("precis", precision)
-def test_unicity_beep_search(cfg: TTCFG, k : int, precis: float) -> None:
+def test_equality_beep_search(cfg: TTCFG, k: int, precis: float) -> None:
+    pcfg = ProbDetGrammar.random(cfg, seed=1)
+    g1 = enumerate(pcfg)
+    g2 = enumerate_prob_grammar(pcfg, k, precis)
+    for p1, p2 in zip(g1, g2):
+        assert (
+            abs(pcfg.probability(p1) - pcfg.probability(p2)) <= 1e-8
+            or abs(pcfg.probability(p1) / pcfg.probability(p2)) <= 1 + precis
+        )
+
+
+@pytest.mark.parametrize("cfg", testdata)
+@pytest.mark.parametrize("k", kvals)
+@pytest.mark.parametrize("precis", precision)
+def test_unicity_beep_search(cfg: TTCFG, k: int, precis: float) -> None:
     pcfg = ProbDetGrammar.uniform(cfg)
     seen = set()
     for program in enumerate_prob_grammar(pcfg, k, precis):
@@ -64,7 +71,7 @@ def test_unicity_beep_search(cfg: TTCFG, k : int, precis: float) -> None:
 @pytest.mark.parametrize("cfg", testdata)
 @pytest.mark.parametrize("k", kvals)
 @pytest.mark.parametrize("precis", precision)
-def test_order_beep_search(cfg: TTCFG, k : int, precis: float) -> None:
+def test_order_beep_search(cfg: TTCFG, k: int, precis: float) -> None:
     pcfg = ProbDetGrammar.random(cfg, seed=4)
     last = 1.0
     for program in enumerate_prob_grammar(pcfg, k, precis):
@@ -75,7 +82,7 @@ def test_order_beep_search(cfg: TTCFG, k : int, precis: float) -> None:
 
 @pytest.mark.parametrize("k", kvals)
 @pytest.mark.parametrize("precis", precision)
-def test_infinite(k : int, precis: float) -> None:
+def test_infinite(k: int, precis: float) -> None:
     pcfg = ProbDetGrammar.random(
         CFG.infinite(dsl, testdata[0].type_request, n_gram=1), 1
     )
@@ -84,7 +91,9 @@ def test_infinite(k : int, precis: float) -> None:
     for program in enumerate_prob_grammar(pcfg, k, precis):
         count -= 1
         p = pcfg.probability(program)
-        assert -1e-12 <= last - p or abs(p / last) <= 1 + precis, f"failed at program n°{count}:{program}, p={p} last={last}, p={np.log(p)} last={np.log(last)}"
+        assert (
+            -1e-12 <= last - p or abs(p / last) <= 1 + precis
+        ), f"failed at program n°{count}:{program}, p={p} last={last}, p={np.log(p)} last={np.log(last)}"
         last = p
         if count < 0:
             break
